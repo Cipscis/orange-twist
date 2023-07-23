6a920e2cf26d6cbd3327ba215393dbfc52a4e507
Mark Hanna <cipscis+github@gmail.com>
2023-07-23 22:18:24 +1200

    Load test data into memory

--- app/assets/js/src/main.ts
+++ app/assets/js/src/main.ts
@@ -1,1 +1,29 @@
+import { DayList } from './types/DayList.js';
+import { TaskStatus } from './types/TaskStatus.js';
 
+const dayList: DayList = {
+	days: [
+		{
+			date: new Date(2023, 7, 23),
+			tasks: [
+				{
+					id: 1,
+					name: 'Example task 2',
+					notes: [],
+					status: TaskStatus.IN_PROGRESS,
+				},
+			],
+		},
+	],
+
+	unfinished: [
+		{
+			id: 0,
+			name: 'Example task',
+			notes: [],
+			status: TaskStatus.TODO,
+		},
+	],
+};
+
+console.log(dayList);
