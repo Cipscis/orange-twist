a1ace21e9f8d501a8f9b1be04b62e5fefea76f36
Brooke Hart <brooke+github@curly.kiwi>
2023-07-26 23:23:19 +1200

    Added standard date formatter

--- app/assets/js/src/renderers/renderDay.ts
+++ app/assets/js/src/renderers/renderDay.ts
@@ -1,10 +1,11 @@
+import { formatDate } from '../formatters/date.js';
 import { Day } from '../types/Day.js';
 import { renderTaskReference } from './renderTaskReference.js';
 
 export function renderDay(day: Day): string {
 	return `
 <li class="day-list__item">
-	<h3>${String(day.date)}</h3>
+	<h3>${formatDate(day.date)}</h3>
 
 	<h4>Tasks</h4>
 	<ul class="task-list">
