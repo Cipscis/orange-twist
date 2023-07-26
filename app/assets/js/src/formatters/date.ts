ba9f32db2b8f2d9db82898bbd3d361f20e6670e8
Mark Hanna <cipscis+github@gmail.com>
2023-07-26 23:23:19 +1200

    Added standard date formatter

---
+++ app/assets/js/src/formatters/date.ts
@@ -0,0 +1,14 @@
+/**
+ * Converts a `Date` to a string matching the short variant
+ * of the {@link https://en.wikipedia.org/wiki/ISO_8601 ISO 8601}
+ * standard: `YYYY-MM-DD`
+ */
+export function formatDate(date: Date): string {
+	const year = String(date.getFullYear()).padStart(2, '0');
+	const month = String(date.getMonth() + 1).padStart(2, '0');
+	const day = String(date.getDate()).padStart(2, '0');
+
+	const dateString = `${year}-${month}-${day}`;
+
+	return dateString;
+}
