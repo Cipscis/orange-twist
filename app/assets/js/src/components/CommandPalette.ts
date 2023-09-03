891a78b82ac97e46486ea92b9e65f9e8b8d537c4
Mark Hanna <cipscis+github@gmail.com>
2023-09-03 12:48:28 +1200

    Clear query when opening command palette

--- app/assets/js/src/components/CommandPalette.ts
+++ app/assets/js/src/components/CommandPalette.ts
@@ -69,6 +69,7 @@ export function CommandPalette(props: CommandPaletteProps) {
 		};
 
 		if (open) {
+			setQuery('');
 			fieldRef.current?.focus();
 			document.addEventListener('keydown', closeOnEscape);
 		}
