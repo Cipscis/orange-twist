4e4d2f6702802909f3411f10283df494d48fb1f1
Mark Hanna <cipscis+github@gmail.com>
2023-11-01 00:06:23 +1300

    More keyboard shortcut tests

---
+++ app/assets/js/src/registers/keyboard-shortcuts/hooks/useKeyboardShortcuts.test.ts
@@ -0,0 +1,66 @@
+import {
+	describe,
+	expect,
+	test,
+} from '@jest/globals';
+
+import { renderHook } from '@testing-library/preact';
+
+import { Command } from '../../commands/index.js';
+
+import { KeyboardShortcutName } from '../types/index.js';
+import { registerKeyboardShortcut } from '../registerKeyboardShortcut.js';
+import { bindKeyboardShortcutToCommand } from '../listeners/index.js';
+
+import { useKeyboardShortcuts } from './useKeyboardShortcuts.js';
+
+describe('useKeyboardShortcuts', () => {
+	test('provides an array containing info on all keyboard shortcuts', () => {
+		const {
+			result,
+			rerender,
+		} = renderHook(() => useKeyboardShortcuts());
+
+		expect(result.current).toMatchObject([]);
+
+		registerKeyboardShortcut(
+			KeyboardShortcutName.DATA_SAVE,
+			[{ key: 's', ctrl: true }]
+		);
+		rerender();
+
+		expect(result.current).toMatchObject([{
+			name: KeyboardShortcutName.DATA_SAVE,
+			shortcuts: [{ key: 's', ctrl: true }],
+			listeners: [],
+		}]);
+
+		bindKeyboardShortcutToCommand(KeyboardShortcutName.DATA_SAVE, Command.DATA_SAVE);
+		// TODO: Re-rendering is not necessary because this mutates the shared `listeners` array
+		// Perhaps `listeners` should be removed from the info made available?
+
+		expect(result.current).toMatchObject([{
+			name: KeyboardShortcutName.DATA_SAVE,
+			shortcuts: [{ key: 's', ctrl: true }],
+		}]);
+		expect(result.current[0].listeners.length).toBe(1);
+
+		registerKeyboardShortcut(
+			KeyboardShortcutName.DATA_SAVE,
+			[
+				{ key: 's', ctrl: true },
+				{ key: 'Enter', ctrl: true },
+			]
+		);
+		rerender();
+
+		expect(result.current).toMatchObject([{
+			name: KeyboardShortcutName.DATA_SAVE,
+			shortcuts: [
+				{ key: 's', ctrl: true },
+				{ key: 'Enter', ctrl: true },
+			],
+		}]);
+		expect(result.current[0].listeners.length).toBe(1);
+	});
+});
---
+++ app/assets/js/src/registers/keyboard-shortcuts/listeners/onNewKeyboardShortcutRegistered.test.ts
@@ -0,0 +1,88 @@
+import {
+	describe,
+	expect,
+	jest,
+	test,
+} from '@jest/globals';
+
+import { KeyboardShortcutName } from '../types/KeyboardShortcutName.js';
+
+import {
+	onNewKeyboardShortcutRegistered,
+	offNewKeyboardShortcutRegistered,
+} from './onNewKeyboardShortcutRegistered.js';
+import { registerKeyboardShortcut } from '../registerKeyboardShortcut.js';
+
+describe('onNewKeyboardShortcutRegistered', () => {
+	test('registers a listener to be fired whenever a keyboard shortcut is registered', () => {
+		const spy = jest.fn();
+		const keyCombo = { key: 'a' };
+
+		onNewKeyboardShortcutRegistered(spy);
+
+		expect(spy).not.toHaveBeenCalled();
+
+		registerKeyboardShortcut(KeyboardShortcutName.DATA_SAVE, [keyCombo]);
+
+		expect(spy).toHaveBeenCalledTimes(1);
+		expect(spy).toHaveBeenCalledWith({
+			name: KeyboardShortcutName.DATA_SAVE,
+			shortcuts: [keyCombo],
+			listeners: [],
+		});
+	});
+
+	test('can be removed by aborting an AbortSignal', () => {
+		const controller = new AbortController();
+		const { signal } = controller;
+
+		const spy = jest.fn();
+
+		onNewKeyboardShortcutRegistered(spy, { signal });
+		registerKeyboardShortcut(KeyboardShortcutName.DATA_SAVE, []);
+
+		expect(spy).toHaveBeenCalledTimes(1);
+
+		controller.abort();
+
+		registerKeyboardShortcut(KeyboardShortcutName.DATA_SAVE, [{ key: 'a' }]);
+
+		expect(spy).toHaveBeenCalledTimes(1);
+	});
+
+	test('doesn\'t bind a listener if an already aborted AbortSignal is passed', () => {
+		const signal = AbortSignal.abort();
+
+		const spy = jest.fn();
+
+		onNewKeyboardShortcutRegistered(spy, { signal });
+		registerKeyboardShortcut(KeyboardShortcutName.DATA_SAVE, []);
+
+		expect(spy).not.toHaveBeenCalled();
+	});
+
+	test('does nothing if the same listener is already bound', () => {
+		const spy = jest.fn();
+
+		onNewKeyboardShortcutRegistered(spy);
+		registerKeyboardShortcut(KeyboardShortcutName.DATA_SAVE, []);
+
+		expect(spy).toHaveBeenCalledTimes(1);
+
+		offNewKeyboardShortcutRegistered(spy);
+
+		registerKeyboardShortcut(KeyboardShortcutName.DATA_SAVE, [{ key: 'a' }]);
+
+		expect(spy).toHaveBeenCalledTimes(1);
+	});
+});
+
+describe('offNewKeyboardShortcutRegistered', () => {
+	test('unbinds a listener bound with onNewKeyboardShortcutRegistered', () => {});
+
+	test('does nothing if the listener is not already bound', () => {
+		expect(
+			() => offNewKeyboardShortcutRegistered(() => {})
+		).not.toThrow();
+	});
+});
