6c657063a132599bf5537bb80de036b77c23787b
Brooke Hart <brooke+github@curly.kiwi>
2023-11-01 00:06:23 +1300

    More keyboard shortcut tests

--- app/assets/js/src/registers/keyboard-shortcuts/getKeyboardShortcuts.test.ts
+++ app/assets/js/src/registers/keyboard-shortcuts/getKeyboardShortcuts.test.ts
@@ -4,16 +4,27 @@ import {
 	test,
 } from '@jest/globals';
 
-import { KeyboardShortcutName } from './types/index.js';
+import { KeyCombo, KeyboardShortcutName } from './types/index.js';
 
 import { registerKeyboardShortcut } from './registerKeyboardShortcut.js';
+import { addKeyboardShortcutListener } from './listeners/addKeyboardShortcutListener.js';
 
 import { getKeyboardShortcuts } from './getKeyboardShortcuts.js';
 
 describe('getKeyboardShortcuts', () => {
 	test('returns a list of all registered keyboard shortcuts', () => {
-		// registerKeyboardShortcut(KeyboardShortcutName.DATA_SAVE, []);
-
 		expect(getKeyboardShortcuts()).toMatchObject([]);
+
+		const keyCombo: KeyCombo = { key: 'a', ctrl: true };
+		const fn = () => {};
+
+		registerKeyboardShortcut(KeyboardShortcutName.DATA_SAVE, [keyCombo]);
+		addKeyboardShortcutListener(KeyboardShortcutName.DATA_SAVE, fn);
+
+		expect(getKeyboardShortcuts()).toMatchObject([{
+			listeners: [fn],
+			name: KeyboardShortcutName.DATA_SAVE,
+			shortcuts: [keyCombo],
+		}]);
 	});
 });
--- app/assets/js/src/registers/keyboard-shortcuts/hooks/useKeyboardShortcuts.ts
+++ app/assets/js/src/registers/keyboard-shortcuts/hooks/useKeyboardShortcuts.ts
@@ -1,4 +1,4 @@
-import { useState } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 
 import { KeyboardShortcutInfo } from '../types/index.js';
 import { onNewKeyboardShortcutRegistered } from '../listeners/index.js';
@@ -9,12 +9,29 @@ export function useKeyboardShortcuts(): ReadonlyArray<Readonly<KeyboardShortcutI
 	// Try to initialise with existing data
 	const [keyboardShortcuts, setKeyboardShortcuts] = useState<ReadonlyArray<Readonly<KeyboardShortcutInfo>>>(getKeyboardShortcuts);
 
-	// Update keyboard shortcuts whenever a new one is added
-	onNewKeyboardShortcutRegistered((newKeyboardShortcut) => {
-		setKeyboardShortcuts(
-			(keyboardShortcuts) => keyboardShortcuts.concat(newKeyboardShortcut)
-		);
-	});
+	useEffect(() => {
+		const controller = new AbortController();
+		const { signal } = controller;
+
+		// Update keyboard shortcuts whenever a new one is added
+		onNewKeyboardShortcutRegistered((newKeyboardShortcut) => {
+			setKeyboardShortcuts((keyboardShortcuts) => {
+				// Start by creating a new array
+				const newKeyboardShortcuts = [...keyboardShortcuts];
+
+				// Then, if there's already an entry with this name, remove it
+				const currentIndex = newKeyboardShortcuts.findIndex(({ name }) => name === newKeyboardShortcut.name);
+				if (currentIndex !== -1) {
+					newKeyboardShortcuts.splice(currentIndex, 1);
+				}
+
+				// Finally, append the new entry to the end
+				return newKeyboardShortcuts.concat(newKeyboardShortcut);
+			});
+		}, { signal });
+
+		return () => controller.abort();
+	}, []);
 
 	return keyboardShortcuts;
 }
