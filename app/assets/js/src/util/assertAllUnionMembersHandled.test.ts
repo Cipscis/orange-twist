825481a7a9b7c781b1f0f5f436d807fc1ceb7520
Brooke Hart <brooke+github@curly.kiwi>
2023-11-04 16:17:21 +1300

    Added tests for `assertAllUnionMembersHandled`

---
+++ app/assets/js/src/util/assertAllUnionMembersHandled.test.ts
@@ -0,0 +1,27 @@
+import {
+	describe,
+	expect,
+	test,
+} from '@jest/globals';
+
+import { assertAllUnionMembersHandled } from './assertAllUnionMembersHandled';
+
+describe('assertAllUnionMembersHandler', () => {
+	test('throws an error', () => {
+		expect(() => {
+			/* @ts-expect-error TypeScript should forbid calling this function except with `never` */
+			assertAllUnionMembersHandled('test');
+		}).toThrowError(
+			'Union member test was not handled.'
+		);
+	});
+
+	test('allows the error message to be customised', () => {
+		expect(() => {
+			assertAllUnionMembersHandled(
+				'test' as never,
+				(value) => `Custom error message with ${value}`
+			);
+		}).toThrowError('Custom error message with test');
+	});
+});
