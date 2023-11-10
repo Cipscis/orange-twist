80bb6943eb95424c117231f48eb07d6af0163bc1
Brooke Hart <brooke+github@curly.kiwi>
2023-11-10 22:55:57 +1300

    Implemented `saveDays` with localStorage persistence

--- app/assets/js/src/registers/days/persistence/saveDays.test.ts
+++ app/assets/js/src/registers/days/persistence/saveDays.test.ts
@@ -1,8 +1,61 @@
 import {
+	beforeEach,
 	describe,
+	expect,
 	test,
 } from '@jest/globals';
 
+import { TaskStatus } from 'types/TaskStatus';
+import { setDayInfo } from '../setDayInfo';
+import { saveDays } from './saveDays';
+
 describe('saveDays', () => {
-	test.todo('returns a Promise that resolves when the content of the days register has been persisted');
+	beforeEach(() => {
+		localStorage.clear();
+	});
+
+	test('returns a Promise that resolves when the content of the days register has been persisted', async () => {
+		const ninthDayInfo = {
+			note: 'Ninth note',
+			tasks: [{
+				id: 1,
+				note: 'Task note',
+				status: TaskStatus.TODO,
+			}],
+		};
+
+		const tenthDayInfo = {
+			note: 'Tenth note',
+			tasks: [
+				{
+					id: 1,
+					note: 'Task note',
+					status: TaskStatus.IN_PROGRESS,
+				},
+				{
+					id: 2,
+					note: 'Second task note',
+					status: TaskStatus.INVESTIGATING,
+				},
+			],
+		};
+
+		setDayInfo('2023-11-09', ninthDayInfo);
+		setDayInfo('2023-11-10', tenthDayInfo);
+
+		expect(localStorage.getItem('days')).toBeNull();
+
+		const saveDaysPromise = saveDays();
+		expect(saveDaysPromise).toBeInstanceOf(Promise);
+
+		expect(localStorage.getItem('days')).toBeNull();
+
+		const saveDaysResult = await saveDaysPromise;
+		expect(saveDaysResult).toBeUndefined();
+
+		expect(localStorage.getItem('days')).toEqual(JSON.stringify([
+			['2023-11-09', { name: '2023-11-09', ...ninthDayInfo }],
+			['2023-11-10', { name: '2023-11-10', ...tenthDayInfo }],
+		]));
+	});
 });
