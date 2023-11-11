593113663de50d4f79599dbe20aa002ab9acc1d4
Mark Hanna <cipscis+github@gmail.com>
2023-11-11 20:10:40 +1300

    Re-implemented `useDayInfo` hook

--- app/assets/js/src/registers/days/hooks/useDayInfo.test.ts
+++ app/assets/js/src/registers/days/hooks/useDayInfo.test.ts
@@ -1,14 +1,89 @@
 import {
+	afterEach,
+	beforeEach,
 	describe,
+	expect,
 	test,
 } from '@jest/globals';
 
+import { renderHook } from '@testing-library/preact';
+
+import { daysRegister } from '../daysRegister';
+import { TaskStatus } from 'types/TaskStatus';
+
 import { useDayInfo } from './useDayInfo';
 
+const ninthDayInfo = {
+	note: 'Ninth note',
+	tasks: [{
+		id: 1,
+		note: 'Task note',
+		status: TaskStatus.TODO,
+	}],
+};
+
+const tenthDayInfo = {
+	note: 'Tenth note',
+	tasks: [
+		{
+			id: 1,
+			note: 'Task note',
+			status: TaskStatus.IN_PROGRESS,
+		},
+		{
+			id: 2,
+			note: 'Second task note',
+			status: TaskStatus.INVESTIGATING,
+		},
+	],
+};
+
 describe('useDayInfo', () => {
-	test.todo('when passed no arguments, returns an array of info on all days');
+	beforeEach(() => {
+		daysRegister.set([
+			['2023-11-09', { name: '2023-11-09', ...ninthDayInfo }],
+			['2023-11-10', { name: '2023-11-10', ...tenthDayInfo }],
+		]);
+	});
 
-	test.todo('when passed a day name that has no matching day, returns null');
+	afterEach(() => {
+		daysRegister.clear();
+	});
 
-	test.todo('when passed a day name that has a matching day, returns that day\'s info');
+	test('when passed no arguments, if there are no days, returns an empty array', () => {
+		daysRegister.clear();
+
+		const { result } = renderHook(() => useDayInfo());
+		expect(result.current).toEqual([]);
+	});
+
+	test('when passed no arguments, returns an array of info on all days', () => {
+		const { result } = renderHook(() => useDayInfo());
+
+		expect(result.current).toEqual([
+			{
+				name: '2023-11-09',
+				...ninthDayInfo,
+			},
+			{
+				name: '2023-11-10',
+				...tenthDayInfo,
+			},
+		]);
+	});
+
+	test('when passed a day name that has no matching day, returns null', () => {
+		const { result } = renderHook(() => useDayInfo('2023-11-08'));
+
+		expect(result.current).toBeNull();
+	});
+
+	test('when passed a day name that has a matching day, returns that day\'s info', () => {
+		const { result } = renderHook(() => useDayInfo('2023-11-09'));
+
+		expect(result.current).toEqual({
+			name: '2023-11-09',
+			...ninthDayInfo,
+		});
+	});
 });
--- app/assets/js/src/registers/days/hooks/useDayInfo.ts
+++ app/assets/js/src/registers/days/hooks/useDayInfo.ts
@@ -1,5 +1,49 @@
+import { useCallback, useEffect, useState } from 'preact/hooks';
+
 import type { DayInfo } from '../types/DayInfo';
 
-export function useDayInfo(dayName?: string): DayInfo | null {
-	return null;
+import { daysRegister } from '../daysRegister';
+import { getDayInfo } from '../getDayInfo';
+
+/**
+ * Provides up to date information on all days.
+ */
+export function useDayInfo(): DayInfo[];
+/**
+ * Provides up to date information on a single day.
+ *
+ * @param dayName The name of the specified day.
+ */
+export function useDayInfo(dayName: string): DayInfo | null;
+export function useDayInfo(dayName?: string): DayInfo[] | DayInfo | null {
+	const getThisDayInfo = useCallback(() => {
+		if (dayName) {
+			return getDayInfo(dayName);
+		} else {
+			return getDayInfo();
+		}
+	}, [dayName]);
+
+	const [dayInfo, setDayInfo] = useState(getThisDayInfo);
+
+	useEffect(() => {
+		const controller = new AbortController();
+		const { signal } = controller;
+
+		daysRegister.addEventListener(
+			'set',
+			() => setDayInfo(getThisDayInfo()),
+			{ signal }
+		);
+
+		daysRegister.addEventListener(
+			'delete',
+			() => setDayInfo(getThisDayInfo()),
+			{ signal }
+		);
+
+		return () => controller.abort();
+	}, [getThisDayInfo]);
+
+	return dayInfo;
 }
