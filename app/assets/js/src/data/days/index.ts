b953fffa1bd7dc764d0d41339c188eb6aa3c1226
Mark Hanna <cipscis@gmail.com>
2023-11-25 15:12:07 +1300

    Split `getDayInfo` into `getDayInfo` and `getAllDayInfo`

--- app/assets/js/src/data/days/index.ts
+++ app/assets/js/src/data/days/index.ts
@@ -3,6 +3,7 @@ export type { DayInfo } from './types';
 export { setDayInfo } from './setDayInfo';
 export { deleteDay } from './deleteDay';
 export { getDayInfo } from './getDayInfo';
+export { getAllDayInfo } from './getAllDayInfo';
 
 export { useDayInfo } from './hooks';
 
