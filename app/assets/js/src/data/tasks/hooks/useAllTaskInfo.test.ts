a91e3d07e7779f5df1ff12ef2ac34ac2777adb67
Mark Hanna <cipscis@gmail.com>
2023-11-26 12:46:54 +1300

    Allow `useAllTaskInfo` to take a matcher function

--- app/assets/js/src/data/tasks/hooks/useAllTaskInfo.test.ts
+++ app/assets/js/src/data/tasks/hooks/useAllTaskInfo.test.ts
@@ -50,63 +50,130 @@ describe('useAllTaskInfo', () => {
 		clear();
 	});
 
-	test('when passed no arguments, if there are no tasks, returns an empty array', () => {
-		tasksRegister.clear();
+	describe('when passed no arguments', () => {
+		test('if there are no tasks, returns an empty array', () => {
+			tasksRegister.clear();
 
-		const { result } = renderHook(() => useAllTaskInfo());
-		expect(result.current).toEqual([]);
-	});
-
-	test('when passed no arguments, returns an array of info on all tasks', () => {
-		const { result } = renderHook(() => useAllTaskInfo());
-
-		expect(result.current).toEqual([firstTaskInfo, secondTaskInfo, thirdTaskInfo]);
-	});
-
-	test('when passed multiple task IDs, returns an array of those tasks\' info', () => {
-		const { result } = renderHook(() => useAllTaskInfo(useMemo(() => [1, 3], [])));
-
-		expect(result.current).toEqual([firstTaskInfo, thirdTaskInfo]);
-	});
-
-	test('when passed multiple task IDs, re-renders only when a matching task is changed', async () => {
-		let renderCount = 0;
-		const { result } = renderHook(() => {
-			renderCount += 1;
-			return useAllTaskInfo(useMemo(() => [1, 3], []));
+			const { result } = renderHook(() => useAllTaskInfo());
+			expect(result.current).toEqual([]);
 		});
 
-		expect(result.current).toEqual([firstTaskInfo, thirdTaskInfo]);
-		expect(renderCount).toBe(1);
+		test('returns an array of info on all tasks', () => {
+			const { result } = renderHook(() => useAllTaskInfo());
 
-		// Updating a different task shouldn't cause re-renders
-		await act(() => setTaskInfo(2, { name: 'New name' }));
-		expect(renderCount).toBe(1);
+			expect(result.current).toEqual([firstTaskInfo, secondTaskInfo, thirdTaskInfo]);
+		});
+	});
 
-		// Updating the watched task should cause re-render with new info
-		await act(() => setTaskInfo(1, { name: 'New name' }));
-		expect(result.current).toEqual([
-			{
-				...firstTaskInfo,
-				name: 'New name',
-			},
-			thirdTaskInfo,
-		]);
-		expect(renderCount).toBe(2);
+	describe('when passed an array of task IDs', () => {
+		test('returns an array of those tasks\' info, excluding non-existed tasks', () => {
+			const { result } = renderHook(() => useAllTaskInfo(useMemo(() => [1, -1, 3], [])));
 
-		// Updating a watched task should cause re-render with new info
-		await act(() => setTaskInfo(3, { name: 'New name' }));
-		expect(result.current).toEqual([
-			{
-				...firstTaskInfo,
-				name: 'New name',
-			},
-			{
-				...thirdTaskInfo,
-				name: 'New name',
-			},
-		]);
-		expect(renderCount).toBe(3);
+			expect(result.current).toEqual([firstTaskInfo, thirdTaskInfo]);
+		});
+
+		test('re-renders only when a matching task is changed', async () => {
+			let renderCount = 0;
+			const { result } = renderHook(() => {
+				renderCount += 1;
+				return useAllTaskInfo(useMemo(() => [1, 3], []));
+			});
+
+			expect(result.current).toEqual([firstTaskInfo, thirdTaskInfo]);
+			expect(renderCount).toBe(1);
+
+			// Updating a different task shouldn't cause re-renders
+			await act(() => setTaskInfo(2, { name: 'New name' }));
+			expect(renderCount).toBe(1);
+
+			// Updating a watched task should cause re-render with new info
+			await act(() => setTaskInfo(1, { name: 'New name' }));
+			expect(result.current).toEqual([
+				{
+					...firstTaskInfo,
+					name: 'New name',
+				},
+				thirdTaskInfo,
+			]);
+			expect(renderCount).toBe(2);
+
+			// Updating a watched task should cause re-render with new info
+			await act(() => setTaskInfo(3, { name: 'New name' }));
+			expect(result.current).toEqual([
+				{
+					...firstTaskInfo,
+					name: 'New name',
+				},
+				{
+					...thirdTaskInfo,
+					name: 'New name',
+				},
+			]);
+			expect(renderCount).toBe(3);
+		});
+	});
+
+	describe('when passed a matcher argument', () => {
+		test('returns an array of matching tasks\' info', () => {
+			const matcher = (task: TaskInfo) => task.status !== TaskStatus.COMPLETED;
+
+			const { result } = renderHook(() => useAllTaskInfo(matcher));
+
+			expect(result.current).toEqual([firstTaskInfo, secondTaskInfo]);
+		});
+
+		test('re-renders only when a matching task is changed', async () => {
+			let renderCount = 0;
+			const matcher = (task: TaskInfo) => task.status !== TaskStatus.COMPLETED;
+
+			const { result } = renderHook(() => {
+				renderCount += 1;
+				return useAllTaskInfo(matcher);
+			});
+
+			expect(result.current).toEqual([firstTaskInfo, secondTaskInfo]);
+			expect(renderCount).toBe(1);
+
+			// Updating an unmatched task shouldn't cause re-renders
+			await act(() => setTaskInfo(3, { name: 'New name' }));
+			expect(renderCount).toBe(1);
+
+			// Updating a matched task should cause re-render with new info
+			await act(() => setTaskInfo(1, { name: 'New name' }));
+			expect(result.current).toEqual([
+				{
+					...firstTaskInfo,
+					name: 'New name',
+				},
+				secondTaskInfo,
+			]);
+			expect(renderCount).toBe(2);
+
+			// Unmatching a matching task should cause re-render without that task
+			await act(() => setTaskInfo(2, { status: TaskStatus.COMPLETED }));
+			expect(result.current).toEqual([
+				{
+					...firstTaskInfo,
+					name: 'New name',
+				},
+			]);
+			expect(renderCount).toBe(3);
+
+			// Matching an unmatched task should cause re-render with that task
+			await act(() => setTaskInfo(3, { status: TaskStatus.TODO }));
+			expect(result.current).toEqual([
+				{
+					...firstTaskInfo,
+					name: 'New name',
+				},
+				{
+					...thirdTaskInfo,
+					name: 'New name',
+					status: TaskStatus.TODO,
+				},
+			]);
+			expect(renderCount).toBe(4);
+		});
 	});
 
 	test('when the argument changes, returns updated information', () => {
