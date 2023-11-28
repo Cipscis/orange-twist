163a97d1591c2de7fd7b9a910783eb9a39beaa87
Brooke Hart <brooke+github@curly.kiwi>
2023-11-28 22:33:42 +1300

    Fixed issue with rendering directly to body

--- app/assets/js/src/ui/prompt/prompt.tsx
+++ app/assets/js/src/ui/prompt/prompt.tsx
@@ -4,13 +4,16 @@ import { getPromiseWithResolver } from 'util/index';
 
 import { ModalPrompt } from './ModalPrompt';
 
+const promptContainer = document.createElement('div');
+document.body.append(promptContainer);
+
 export async function prompt(message: string): Promise<string | null> {
 	const [resultPromise, resolve] = getPromiseWithResolver<string | null>();
 
 	render(<ModalPrompt
 		message={message}
 		resolve={resolve}
-	/>, document.body);
+	/>, promptContainer);
 
 	return resultPromise;
 }
