a8f3f248b876c2f0ef6f27393bd586ebd995a81b
Brooke Hart <brooke+github@curly.kiwi>
2023-12-07 22:09:23 +1300

    Self PR review

--- app/assets/js/src/dom-lib/CloseWatcher.d.ts
+++ app/assets/js/src/dom-lib/CloseWatcher.d.ts
@@ -1,19 +1,24 @@
-/* eslint-disable @typescript-eslint/no-explicit-any, no-var */
-
 interface CloseWatcherOptions {
+	/**
+	 * If the `signal` option is provided, then *watcher* can be destroyed (as if by `watcher.destroy()`) by aborting the given `AbortSignal`.
+	 */
 	signal?: AbortSignal;
 }
 
-interface CloseWatcher extends EventTarget {
-	requestClose(): void;
-	close(): void;
-	destroy(): void;
-
-	oncancel: ((this: GlobalEventHandlers, ev: Event) => any) | null;
-	onclose: ((this: GlobalEventHandlers, ev: Event) => any) | null;
-}
-
 declare global {
+	interface CloseWatcher extends EventTarget {
+		requestClose(): void;
+		close(): void;
+		destroy(): void;
+
+		// Following the pattern from built-in TypeScript
+		// DOM API types, which use explicit `any`
+		/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+		oncancel: ((this: GlobalEventHandlers, ev: Event) => any) | null;
+		/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+		onclose: ((this: GlobalEventHandlers, ev: Event) => any) | null;
+	}
+
 	interface Window {
 		/**
 		 * [HTML Specification](https://html.spec.whatwg.org/multipage/interaction.html#the-closewatcher-interface)
