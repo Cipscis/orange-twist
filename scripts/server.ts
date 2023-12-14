6d6c5734ade4f9cacb5f8dfc352dec3292f6c0ef
Mark Hanna <cipscis@gmail.com>
2023-12-14 22:53:38 +1300

    Updated dev server to reflect GitHub Pags environment

--- scripts/server.ts
+++ scripts/server.ts
@@ -7,21 +7,35 @@ const app = express();
 const port = process.env.PORT;
 const projectName = process.env.PROJECT_NAME;
 
-app.use(express.static('app'));
-
 if (projectName) {
 	// GitHub Pages publishes projects to <username>.github.io/<projectname>
 	// This breaks root-relative URLs, so instead use "/projectname/path/" locally
 	// and resolve it by redirecting it here to a root relative path.
+	const ghPagesPathPattern = new RegExp(`^/${projectName}(/|$)`, 'i');
 
-	const ghPagesPathPattern = new RegExp(`^/${projectName}/`, 'i');
-	app.get(ghPagesPathPattern, (req, res) => {
-		const path = req.url.replace(ghPagesPathPattern, '/');
-		const url = `http://${req.headers.host}${path}`;
+	app.use((request, res, next) => {
+		if (!ghPagesPathPattern.test(request.url)) {
+			res.status(404);
+			res.send(`<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Error</title>
+	</head>
+	<body>
+		<pre>Cannot GET ${request.url}
+Did you mean <a href="/${projectName}${request.url}">/${projectName}${request.url}</a>?</pre>
+	</body>
+</html>`);
+			return;
+		}
 
-		res.redirect(url);
+		request.url = request.url.replace(ghPagesPathPattern, '/');
+		next();
 	});
 }
+
+app.use(express.static('app'));
 
 app.listen(port, () => {});
 console.log(`Listening on port ${port}`);
