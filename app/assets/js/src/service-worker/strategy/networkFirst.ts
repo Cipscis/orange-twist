7ae8165c8be25c619346316d3f1a6ee65a12f738
Brooke Hart <brooke+github@curly.kiwi>
2023-12-15 20:31:49 +1300

    Use 408 response for network failure with service worker

--- app/assets/js/src/service-worker/strategy/networkFirst.ts
+++ app/assets/js/src/service-worker/strategy/networkFirst.ts
@@ -33,9 +33,15 @@ export async function networkFirst({ preloadResponse, request }: FetchEvent): Pr
 		if (request.destination === 'document') {
 			// If the request is for a page, and we don't have a cached
 			// response, try to return a generic network error page
-			const errorResponse = await getCachedResponse(new URL('/orange-twist/404.html', self.location.origin));
+			const errorResponse = await getCachedResponse(
+				new URL('/orange-twist/408.html', self.location.origin)
+			);
+
 			if (errorResponse) {
-				return errorResponse;
+				return new Response(errorResponse.clone().body, {
+					...errorResponse,
+					status: 408, // REQUEST_TIMEOUT
+				});
 			}
 		}
 
