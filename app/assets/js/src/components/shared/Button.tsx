274abc24a6588e8ebf6d70ca75e851a5b9c93225
Mark Hanna <cipscis@gmail.com>
2023-12-16 15:01:13 +1300

    Allow <Button> to be a link

--- app/assets/js/src/components/shared/Button.tsx
+++ app/assets/js/src/components/shared/Button.tsx
@@ -17,20 +17,42 @@ function getVariantClass(variant: ButtonVariant): string {
 	}
 }
 
-interface ButtonProps extends JSX.HTMLAttributes<HTMLButtonElement> {
+// <Button> can contain a <button> or an <a>, so inherit from both
+type ButtonPropsBase = JSX.HTMLAttributes<HTMLButtonElement> &
+	JSX.HTMLAttributes<HTMLAnchorElement>;
+
+interface ButtonProps extends ButtonPropsBase {
 	class?: string;
 	variant?: ButtonVariant;
+
+	href?: string;
 }
 
 export function Button(props: ButtonProps): JSX.Element {
-	return <button
-		type="button"
-		{...props}
-		class={classNames(
-			props.class,
-			getVariantClass(props.variant ?? ButtonVariant.PRIMARY),
-		)}
-	>
-		{props.children}
-	</button>;
+	const {
+		href,
+
+		...passthroughProps
+	} = props;
+
+	const classString = classNames(
+		props.class,
+		getVariantClass(props.variant ?? ButtonVariant.PRIMARY),
+	);
+
+	return href
+		? <a
+			href={href}
+			{...passthroughProps}
+			class={classString}
+		>
+			{props.children}
+		</a>
+		: <button
+			type="button"
+			{...passthroughProps}
+			class={classString}
+		>
+			{props.children}
+		</button>;
 }
