217f0930cedfe85aaaa9d831f461ccb4f0a78cdf
Mark Hanna <cipscis@gmail.com>
2024-03-10 12:48:17 +1300

    74 Don't display "first visit" UI until loading is complete

--- app/assets/js/src/components/days/DaysList.tsx
+++ app/assets/js/src/components/days/DaysList.tsx
@@ -1,11 +1,12 @@
 import { type JSX, h } from 'preact';
-import { useCallback, useMemo } from 'preact/hooks';
+import { useCallback, useContext, useMemo } from 'preact/hooks';
 
 import { Command } from 'types/Command';
 import { fireCommand } from 'registers/commands';
 
 import { useAllDayInfo } from 'data';
 
+import { OrangeTwistContext } from '../OrangeTwistContext';
 import { Button } from '../shared';
 import { Day } from './Day';
 
@@ -15,6 +16,8 @@ import { Day } from './Day';
 export function DayList(): JSX.Element {
 	const unsortedDays = useAllDayInfo();
 
+	const { isLoading } = useContext(OrangeTwistContext);
+
 	const days = useMemo(() => unsortedDays.sort(
 		({ name: nameA }, { name: nameB }) => nameA.localeCompare(nameB)
 	), [unsortedDays]);
@@ -22,7 +25,7 @@ export function DayList(): JSX.Element {
 	return <section class="orange-twist__section">
 		<h2 class="orange-twist__title">Days</h2>
 
-		{days.length <= 1 && (
+		{!isLoading && days.length <= 1 && (
 			<p>If you need help getting started, try <a href="/help">the help page</a>.</p>
 		)}
 
