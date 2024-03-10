f1ea1acef230ac9e60919045399df7f0eb4ecc39
Brooke Hart <brooke+github@curly.kiwi>
2024-03-10 15:32:22 +1300

    77 Added tests covering new sorting behaviour

--- app/assets/js/src/components/tasks/CompletedTaskList.test.tsx
+++ app/assets/js/src/components/tasks/CompletedTaskList.test.tsx
@@ -12,7 +12,11 @@ import '@testing-library/jest-dom/jest-globals';
 import { cleanup, render } from '@testing-library/preact';
 
 import { TaskStatus } from 'types/TaskStatus';
-import { clear, setTaskInfo } from 'data';
+import {
+	clear,
+	setDayTaskInfo,
+	setTaskInfo,
+} from 'data';
 
 import { CompletedTaskList } from './CompletedTaskList';
 
@@ -25,20 +29,59 @@ describe('CompletedTaskList', () => {
 			status: TaskStatus.IN_PROGRESS,
 			sortIndex: 1,
 		});
+		setDayTaskInfo({
+			taskId: 1,
+			dayName: '2024-03-04',
+		}, {
+			status: TaskStatus.IN_PROGRESS,
+		});
+
 		setTaskInfo(2, {
 			name: 'Task two',
 			status: TaskStatus.COMPLETED,
 			sortIndex: 3,
 		});
+		setDayTaskInfo({
+			taskId: 2,
+			dayName: '2024-03-03',
+		}, {
+			status: TaskStatus.COMPLETED,
+		});
+
 		setTaskInfo(3, {
 			name: 'Task three',
 			status: TaskStatus.WILL_NOT_DO,
 			sortIndex: 2,
 		});
+		setDayTaskInfo({
+			taskId: 3,
+			dayName: '2024-03-02',
+		}, {
+			status: TaskStatus.WILL_NOT_DO,
+		});
+
 		setTaskInfo(4, {
 			name: 'Task four',
 			status: TaskStatus.INVESTIGATING,
 			sortIndex: -1,
+		});
+		setDayTaskInfo({
+			taskId: 4,
+			dayName: '2024-03-01',
+		}, {
+			status: TaskStatus.INVESTIGATING,
+		});
+
+		setTaskInfo(5, {
+			name: 'Task five',
+			status: TaskStatus.COMPLETED,
+			sortIndex: -1,
+		});
+		setDayTaskInfo({
+			taskId: 5,
+			dayName: '2024-03-02',
+		}, {
+			status: TaskStatus.COMPLETED,
 		});
 	});
 
@@ -51,8 +94,20 @@ describe('CompletedTaskList', () => {
 
 		expect(queryByText('Task two')).toBeInTheDocument();
 		expect(queryByText('Task three')).toBeInTheDocument();
+		expect(queryByText('Task five')).toBeInTheDocument();
 
 		expect(queryByText('Task one')).not.toBeInTheDocument();
 		expect(queryByText('Task four')).not.toBeInTheDocument();
+	});
+
+	test('renders completed tasks in order of completion', () => {
+		const { queryAllByText } = render(<CompletedTaskList />);
+
+		const tasks = queryAllByText(/^Task /);
+		expect(tasks.map(({ textContent }) => textContent)).toEqual([
+			'Task five',
+			'Task three',
+			'Task two',
+		]);
 	});
 });
