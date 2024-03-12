585dcfef6ccc0c0a5c056c48596186c9ae25ccf8
Brooke Hart <brooke+github@curly.kiwi>
2024-03-12 19:54:16 +1300

    36 Added task link shortcode

--- app/assets/js/src/components/shared/Markdown/Markdown.tsx
+++ app/assets/js/src/components/shared/Markdown/Markdown.tsx
@@ -1,11 +1,13 @@
 import { h, type JSX } from 'preact';
+import { useLayoutEffect, useRef } from 'preact/hooks';
 
 import { classNames } from 'util/index';
 
-import { marked } from 'marked';
-import { useLayoutEffect, useRef } from 'preact/hooks';
+import { marked, type Tokens } from 'marked';
 import { markedHighlight } from 'marked-highlight';
 import hljs from 'highlight.js';
+import { getTaskInfo } from 'data';
+import { TaskStatusSymbol } from 'types/TaskStatus';
 
 interface MarkdownProps extends h.JSX.HTMLAttributes<HTMLDivElement> {
 	/**
@@ -25,6 +27,7 @@ marked.use({
 	renderer: {
 		link(href, title, text) {
 			try {
+				// Render external links in a new tab
 				const url = new URL(href, location.origin);
 				const isExternal = url.origin !== location.origin;
 
@@ -47,6 +50,59 @@ marked.use({
 			}
 		},
 	},
+	extensions: [
+		{
+			name: 'taskLink',
+			level: 'inline',
+			start(src) {
+				// Tell Marked when to stop and check for a match
+				return src.match(/\[\[\d/)?.index;
+			},
+			tokenizer(src, tokens) {
+				// Expression for complete token, from its start
+				// Match a number in double square braces, and collect any following text
+				// e.g. "[[48]]"
+				const rule = /^(\[\[(\d+)\]\])(.*?)(?:$|\n)/;
+				const match = rule.exec(src);
+				if (!match) {
+					return undefined;
+				}
+
+				const token = {
+					type: 'taskLink',
+					raw: match[0],
+					taskId: match[2],
+					remainder: match[3],
+					tokens: [],
+				};
+				// Tell the lexer to parse anything in the remainder
+				this.lexer.inline(token.remainder, token.tokens);
+				return token;
+			},
+			renderer(token) {
+				const taskLink = (() => {
+					const taskId = Number(token.taskId);
+					if (isNaN(taskId)) {
+						// TODO: Needs styling
+						return `Invalid task ID ${token.taskId}`;
+					}
+
+					const taskInfo = getTaskInfo(taskId);
+					if (!taskInfo) {
+						// TODO: Needs styling
+						return `No task with ID ${taskId}`;
+					}
+
+					// TODO: Needs styling
+					const taskLink = `<a href="/task?id=${token.taskId}" class="task-link"><span class="task-link__status">${TaskStatusSymbol[taskInfo.status]}</span> <span class="task-link__name">${taskInfo.name}</span></a>`;
+					return taskLink;
+				})()
+
+				const remainder = this.parser.parseInline(token.tokens ?? []);
+				return `${taskLink}${remainder}`;
+			},
+		},
+	],
 });
 
 marked.use(markedHighlight({
