61cbbaf0321e0dad33c38f17e91f5693260d1c65
Mark Hanna <cipscis@gmail.com>
2024-04-13 13:18:19 +1200

    124 Fix `animate` function to properly consume `CSSStyleDeclaration`

--- app/assets/js/src/utils/animate/cssKeyframesToJsKeyframes.ts
+++ app/assets/js/src/utils/animate/cssKeyframesToJsKeyframes.ts
@@ -1,45 +1,52 @@
 /**
+ * Convert a string to a number, if possible, otherwise return the string.
+ */
+function getTypedValue(value: string): string | number {
+	const numValue = Number(value);
+	if (!isNaN(numValue)) {
+		return numValue;
+	}
+	return value;
+}
+
+/**
+ * Convert an array-like `CSSStyleDeclaration` into an easier to
+ * work with regular object that can be used as part of a `Keyframe`.
+ */
+function cssStyleDeclarationToObject(style: CSSStyleDeclaration): Record<string, string | number> {
+	const entries: [string, string | number][] = [];
+
+	const stylesLength = style.length;
+	for (let i = 0; i < stylesLength; i++) {
+		const property = style.item(i);
+		const value = getTypedValue(
+			style.getPropertyValue(property)
+		);
+		// Ignore property priority
+
+		entries.push([property, value]);
+	}
+
+	return Object.fromEntries(entries);
+}
+
+/**
+ * Convert a CSS keyframe's offset string, e.g. "50%", into a number
+ * in the 0-1 range that can be used as the `offset` property of a
+ * JS keyframe.
+ */
+function getCssKeyframeOffset(cssKeyframe: CSSKeyframeRule): number {
+	const keytextNumber = Number(cssKeyframe.keyText.replace(/%$/, '')) / 100;
+	return keytextNumber;
+}
+
+/**
  * Converts a `CSSKeyframeRule` to a `Keyframe` object that can be
  * used with `Element.animate`.
  */
 function cssKeyframeRuleToKeyframe(cssKeyframe: CSSKeyframeRule): Keyframe {
-	// Use destructuring and spread syntax to remove unwanted properties
-	const {
-		parentRule,
-		getPropertyPriority, /* eslint-disable-line @typescript-eslint/unbound-method */
-		getPropertyValue, /* eslint-disable-line @typescript-eslint/unbound-method */
-		item, /* eslint-disable-line @typescript-eslint/unbound-method */
-		removeProperty, /* eslint-disable-line @typescript-eslint/unbound-method */
-		setProperty, /* eslint-disable-line @typescript-eslint/unbound-method */
-		[Symbol.iterator]: _ignore,
-
-		...keyframeStyles
-	} = cssKeyframe.style;
-
-	// Remove further unwanted properties, and clean some others
-	const cleanKeyframeStyles = Object.fromEntries(
-		Object.entries(keyframeStyles)
-			// Remove empty CSS properties and numeric keys
-			.filter(
-				([key, value]) => value !== '' && isNaN(Number(key))
-			)
-			// Convert number values to actual numbers
-			.map(
-				([key, value]) => {
-					if (isNaN(Number(value))) {
-						return [key, value];
-					}
-
-					return [key, Number(value)];
-				}
-			)
-	);
-
-	/** The `keyText` property of the CSS keyframe,
-	 * converted to a number in the 0-1 range so
-	 * it can be used as the `offset` propert of
-	 * a JS keyframe. */
-	const keyTextNumber = Number(cssKeyframe.keyText.replace(/%$/, '')) / 100;
+	const cleanKeyframeStyles = cssStyleDeclarationToObject(cssKeyframe.style);
+	const keyTextNumber = getCssKeyframeOffset(cssKeyframe);
 
 	const jsKeyframe = {
 		...cleanKeyframeStyles,
