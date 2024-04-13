e70e70a90affe9ae7904fca188ac65159ea798b8
Mark Hanna <cipscis@gmail.com>
2024-04-13 13:28:01 +1200

    121 Version update

--- app/assets/js/src/components/Footer.tsx
+++ app/assets/js/src/components/Footer.tsx
@@ -7,7 +7,7 @@ export function Footer(): JSX.Element {
 	return <footer class="orange-twist__footer content">
 		<div class="orange-twist__footer-row">
 			<span class="orange-twist__footer-version">by Mark Hanna</span>
-			<span class="orange-twist__footer-version">version 1.3.1</span>
+			<span class="orange-twist__footer-version">version 1.3.2</span>
 		</div>
 		<div class="orange-twist__footer-row">
 			<a href="/help/">
