32947cb99d7ecf3c3a226f977824e2c4f8d2ffe6
Mark Hanna <cipscis@gmail.com>
2024-04-13 14:00:47 +1200

    99 PR self-review

--- app/assets/js/src/data/templates/deleteTemplate.ts
+++ app/assets/js/src/data/templates/deleteTemplate.ts
@@ -1,7 +1,7 @@
 import { templatesRegister } from './templatesRegister';
 
 /**
- * Delete's all information associated with a template.
+ * Deletes all information associated with a template.
  *
  * @param id The ID specifying the template to delete.
  */
