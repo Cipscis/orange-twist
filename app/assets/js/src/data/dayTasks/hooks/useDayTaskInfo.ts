978e30f71b330002a03763638b481c11a5a0f585
Mark Hanna <cipscis@gmail.com>
2024-04-21 16:09:15 +1200

    Use `useRegister` hook in register-specific hooks

--- app/assets/js/src/data/dayTasks/hooks/useDayTaskInfo.ts
+++ app/assets/js/src/data/dayTasks/hooks/useDayTaskInfo.ts
@@ -1,88 +1,28 @@
 import {
 	useCallback,
-	useEffect,
-	useLayoutEffect,
-	useRef,
-	useState,
 } from 'preact/hooks';
+
+import { useRegister } from 'utils/register';
 
 import {
 	type DayTaskIdentifier,
 	type DayTaskInfo,
 } from '../types';
-import type { RegisterKey } from 'utils';
 
 import { dayTasksRegister } from '../dayTasksRegister';
-import { decodeDayTaskKey } from '../util';
-import { getDayTaskInfo } from '../getDayTaskInfo';
+import { encodeDayTaskKey } from '../util';
 
 /**
  * Provides up to date information on a single day task.
  */
 export function useDayTaskInfo(identifier: DayTaskIdentifier): DayTaskInfo | null {
-	const [thisDayTaskInfo, setThisDayTaskInfo] = useState(() => getDayTaskInfo(identifier));
+	const identifierKey = encodeDayTaskKey(identifier);
 
-	const doneInitialRender = useRef(false);
-
-	// Update thisDayTaskInfo if the identifier changes
-	useEffect(() => {
-		// Don't re-set the state during initial render
-		if (!doneInitialRender.current) {
-			doneInitialRender.current = true;
-			return;
-		}
-
-		setThisDayTaskInfo(getDayTaskInfo(identifier));
-	}, [identifier]);
-
-	/**
-	 * Update the day task info if and only if a relevant day task has changed.
-	 */
-	const handleDayTaskInfoUpdate = useCallback((
-		changes: { key: RegisterKey<typeof dayTasksRegister>; }[]
-	) => {
-		const hasChanged = Boolean(changes.find(
-			({ key }) => {
-				const { dayName, taskId } = decodeDayTaskKey(key);
-
-				if (
-					dayName !== identifier.dayName ||
-					taskId !== identifier.taskId
-				) {
-					return false;
-				}
-
-				return true;
-			}
-		));
-
-		if (hasChanged) {
-			setThisDayTaskInfo(getDayTaskInfo(identifier));
-		}
-	}, [identifier]);
-
-	// Listen for relevant changes on dayTasksRegister
-	// Use a layout effect so it doesn't wait for rendering,
-	// otherwise data could finish loading after we've read
-	// it but before we start listening for changes.
-	useLayoutEffect(() => {
-		const controller = new AbortController();
-		const { signal } = controller;
-
-		dayTasksRegister.addEventListener(
-			'set',
-			handleDayTaskInfoUpdate,
-			{ signal }
-		);
-
-		dayTasksRegister.addEventListener(
-			'delete',
-			handleDayTaskInfoUpdate,
-			{ signal }
-		);
-
-		return () => controller.abort();
-	}, [handleDayTaskInfoUpdate]);
-
-	return thisDayTaskInfo;
+	return useRegister(
+		dayTasksRegister,
+		useCallback(
+			(key) => key === identifierKey,
+			[identifierKey]
+		)
+	)[0] ?? null;
 }
--- app/assets/js/src/data/templates/hooks/useTemplateInfo.ts
+++ app/assets/js/src/data/templates/hooks/useTemplateInfo.ts
@@ -1,16 +1,11 @@
 import {
 	useCallback,
-	useEffect,
-	useLayoutEffect,
-	useRef,
-	useState,
 } from 'preact/hooks';
 
+import { useRegister } from 'utils/register';
+
 import { templatesRegister } from '../templatesRegister';
-import {
-	type TemplateInfo,
-	getTemplateInfo,
-} from 'data';
+import { type TemplateInfo } from 'data';
 
 /**
  * Provides up to date information on a single template.
@@ -18,55 +13,8 @@ import {
  * @param templateId The name of the specified template.
  */
 export function useTemplateInfo(templateId: number): TemplateInfo | null {
-	// Initialise thisTemplateInfo based on the passed templateId
-	const [thisTemplateInfo, setThisTemplateInfo] = useState(() => getTemplateInfo(templateId));
-
-	const doneInitialRender = useRef(false);
-
-	// Update thisTemplateInfo if templateId changes
-	useEffect(() => {
-		// Don't re-set the state during the initial render
-		if (!doneInitialRender.current) {
-			doneInitialRender.current = true;
-			return;
-		}
-
-		setThisTemplateInfo(getTemplateInfo(templateId));
-	}, [templateId]);
-
-	/**
-	 * Update the template info if and only if the relevant template has changed.
-	 */
-	const handleTemplateInfoUpdate = useCallback((changes: { key: number; }[]) => {
-		const hasChanged = Boolean(changes.find(({ key }) => key === templateId));
-		if (hasChanged) {
-			const updatedTemplateInfo = getTemplateInfo(templateId);
-			setThisTemplateInfo(updatedTemplateInfo);
-		}
-	}, [templateId]);
-
-	// Listen for relevant changes on templatesRegister
-	// Use a layout effect so it doesn't wait for rendering,
-	// otherwise data could finish loading after we've read
-	// it but before we start listening for changes.
-	useLayoutEffect(() => {
-		const controller = new AbortController();
-		const { signal } = controller;
-
-		templatesRegister.addEventListener(
-			'set',
-			handleTemplateInfoUpdate,
-			{ signal }
-		);
-
-		templatesRegister.addEventListener(
-			'delete',
-			handleTemplateInfoUpdate,
-			{ signal }
-		);
-
-		return () => controller.abort();
-	}, [handleTemplateInfoUpdate]);
-
-	return thisTemplateInfo;
+	return useRegister(
+		templatesRegister,
+		useCallback((key) => key === templateId, [templateId])
+	)[0] ?? null;
 }
