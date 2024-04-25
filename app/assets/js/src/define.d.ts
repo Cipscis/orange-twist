8c229cf1311f25e69dc4a5be73ecde4fa11bc4a5
Brooke Hart <brooke+github@curly.kiwi>
2024-04-26 09:45:37 +1200

    Lengthened comment line length

--- app/assets/js/src/define.d.ts
+++ app/assets/js/src/define.d.ts
@@ -1,12 +1,9 @@
 /**
- * @file This file declares constants
- * that are replaced during the build process.
+ * @file This file declares constants that are replaced during the build process.
  */
 
-/** The current version of the app,
- * read out of the package.json file. */
+/** The current version of the app, read out of the package.json file. */
 declare const __VERSION__: string;
 
-/** Whether or not the current build
- * is in development mode. */
+/** Whether or not the current build is in development mode. */
 declare const __IS_DEV__: boolean;
