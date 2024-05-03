d60505bf2300d5f2769f4e822c9c26869ae21021
Mark Hanna <mark.hanna@gettimely.com>
2024-05-03 18:02:28 +1200

    Added fps display in dev mode

--- app/assets/js/src/enhancements.ts
+++ app/assets/js/src/enhancements.ts
@@ -11,10 +11,85 @@ if (navigator.serviceWorker) {
 }
 
 if (__IS_DEV__) {
-	document.title += ' (Next)';
+	/**
+	 * Update the document title and favicon to reflect that the app is running in dev mode.
+	 */
+	const displayDevMode = () => {
+		document.title += ' (Next)';
 
-	const favicon = document.querySelector<HTMLLinkElement>('link[rel="icon"]');
-	if (favicon) {
-		favicon.href = '/favicon-dev.svg';
-	}
+		const favicon = document.querySelector<HTMLLinkElement>('link[rel="icon"]');
+		if (favicon) {
+			favicon.href = '/favicon-dev.svg';
+		}
+	};
+
+	/**
+	 * Add a frames per second display to the UI.
+	 */
+	const displayFramesPerSecond = () => {
+		const fpsEl = Object.assign(
+			document.createElement('span'),
+			{
+				id: 'fps',
+				className: 'fps',
+			} satisfies Partial<HTMLSpanElement>
+		);
+		document.body.prepend(fpsEl);
+
+		const fpsTextLowest = Object.assign(
+			document.createElement('span'),
+			{
+				className: 'fps__number fps__lowest',
+				innerText: '-',
+			} satisfies Partial<HTMLSpanElement>
+		);
+		const fpsTextCurrent = Object.assign(
+			document.createElement('span'),
+			{
+				className: 'fps__number fps__current',
+				innerText: '-',
+			} satisfies Partial<HTMLSpanElement>
+		);
+		fpsEl.append(fpsTextLowest, fpsTextCurrent);
+
+		const fpsLog: number[] = [];
+
+		let previousTime = performance.now();
+		const displayFps = (time: number) => {
+			const dt = time - previousTime;
+			previousTime = time;
+
+			const fps = Math.floor(1000 / dt);
+			if (fps <= 0) {
+				// Assume this means the tab was inactive, so skip this frame
+				requestAnimationFrame(displayFps);
+				return;
+			}
+
+			fpsLog.push(fps);
+			if (fpsLog.length > 120) {
+				fpsLog.shift();
+			}
+
+			const fpsLowest = Math.min(...fpsLog);
+			const fpsLowestString = String(fpsLowest);
+			if (fpsTextLowest.innerText !== fpsLowestString) {
+				fpsTextLowest.innerText = fpsLowestString;
+			}
+
+			const fpsCurrentString = String(fps);
+			if (fpsTextCurrent.innerText !== fpsCurrentString) {
+				fpsTextCurrent.innerText = fpsCurrentString;
+			}
+
+			const isLowFps = fpsLowest < 30;
+			fpsEl.classList.toggle('fps--low', isLowFps);
+
+			requestAnimationFrame(displayFps);
+		};
+		requestAnimationFrame(displayFps);
+	};
+
+	displayDevMode();
+	displayFramesPerSecond();
 }
