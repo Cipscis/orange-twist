23761950b5174f152636f5e447ce238bdd66304e
Brooke Hart <brooke+github@curly.kiwi>
2024-06-01 10:46:06 +1200

    mobile task reordering button icon

--- app/assets/js/src/components/shared/DragList/DragControls.tsx
+++ app/assets/js/src/components/shared/DragList/DragControls.tsx
@@ -1,6 +1,8 @@
 import { h, type JSX } from 'preact';
 import { useCallback } from 'preact/hooks';
 
+import { IconButton } from '../IconButton';
+
 interface DragControlsProps {
 	dragStartHandler: (e: DragEvent) => void;
 	moveItemBy: (itemKey: number, offset: number) => void;
@@ -21,11 +23,6 @@ export function DragControls(props: DragControlsProps): JSX.Element {
 		[moveItemBy, itemKey]
 	);
 
-	const moveDown = useCallback(
-		() => moveItemBy(itemKey, +1),
-		[moveItemBy, itemKey]
-	);
-
 	return <>
 		{/* Drag handle for fine pointer devices */}
 		<span
@@ -34,20 +31,13 @@ export function DragControls(props: DragControlsProps): JSX.Element {
 			onDragStart={dragStartHandler}
 		/>
 
-		{/* Up and down buttons for coarse pointer devices */}
-		<div class="drag-list__move-buttons">
-			<button
-				type="button"
+		{/* Buttons for coarse pointer devices */}
+		<div class="drag-list__move-button">
+			<IconButton
+				icon="&#x2303;"
 				title="Move up"
 				onClick={moveUp}
-				class="drag-list__move-button"
-			>&#x2303;</button>
-			<button
-				type="button"
-				title="Move down"
-				onClick={moveDown}
-				class="drag-list__move-button"
-			>&#x2304;</button>
+			/>
 		</div>
 	</>;
 }
