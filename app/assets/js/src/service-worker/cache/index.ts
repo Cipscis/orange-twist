c9e8a4510b0fe7917f2240746bf410c05ddade0b
Mark Hanna <cipscis@gmail.com>
2024-08-05 21:29:27 +1200

    Updated `cacheFirst` strategy to refresh the cache instead of clearing it

--- app/assets/js/src/service-worker/cache/index.ts
+++ app/assets/js/src/service-worker/cache/index.ts
@@ -1,5 +1,5 @@
 export { addToCache } from './addToCache';
-export { clearCache } from './clearCache';
 export { deleteOldCaches } from './deleteOldCaches';
 export { getCachedResponse } from './getCachedResponse';
 export { populateCache } from './populateCache';
+export { refreshCache } from './refreshCache';
--- app/assets/js/src/service-worker/strategy/cacheFirst.ts
+++ app/assets/js/src/service-worker/strategy/cacheFirst.ts
@@ -1,26 +1,35 @@
 import {
 	addToCache,
-	clearCache,
 	getCachedResponse,
+	refreshCache,
 } from '../cache';
 import { getFallbackResponse } from '../error';
 
 /**
  * Return a cached `Response` if possible, while waiting for the network response.
  *
- * If an update is detected from a network response, clear the entire cache so fresh assets will be requested and cached.
+ * If an update is detected from a network response, refresh the entire cache by requesting
+ * a fresh copy of each entry that already exists in the cache. Any entries that fail to be
+ * refreshed in this way, for example due to a network error, will be cleared so fresh assets
+ * will be requested and cached.
+ *
+ * The cache is refreshed asynchronously, without blocking the current request. It's assumed
+ * that responses served during a document load are all requested up front, and will be
+ * retrieved from the cache faster than the cache is able to be refreshed. So long as this
+ * assumption holds, each document load should consist entirely of either stale or fresh assets.
+ *
+ * ---
+ *
+ * This strategy relies on the assumption that all network requests are for static assets, so
+ * if the response of any request has changed then any number of others *may* have changed.
+ *
+ * This strategy also relies less heavily on the assumption that there is a relatively small
+ * number and total size of these static assets, as this affects how heavy the act of refreshing
+ * the cache is.
  */
-export async function cacheFirst({ preloadResponse, request }: FetchEvent): Promise<Response> {
+export async function cacheFirst({ request }: FetchEvent): Promise<Response> {
 	// Initialise the network request immediately, and queue its follow-up action to happen asynchronously
-	const networkPromise = (async () => {
-		// If there is a preloaded response, prefer that
-		const preloadedNetworkResponse = await preloadResponse as unknown;
-		if (preloadedNetworkResponse instanceof Response) {
-			return preloadedNetworkResponse;
-		}
-
-		return fetch(request);
-	})();
+	const networkPromise = fetch(request);
 	networkPromise.then(
 		(response) => handleNetworkResponse(request, response)
 	);
@@ -41,7 +50,7 @@ export async function cacheFirst({ preloadResponse, request }: FetchEvent): Prom
 }
 
 /**
- * Adds a successful response to the cache. If it's changed since it was last cached, also clears the cache.
+ * Adds a successful response to the cache. If it's changed since it was last cached, also refreshes the cache.
  *
  * Ignores unsuccessful responses.
  */
@@ -49,8 +58,8 @@ async function handleNetworkResponse(request: Request, response: Response) {
 	if (response.ok) {
 		const responseToCache = response.clone();
 
-		// Check if a response has changes since it was last cached, and clear the cache if so
-		const shouldClearCache = await (async () => {
+		// Check if a response has changes since it was last cached, and refresh the cache if so
+		const shouldRefreshCache = await (async () => {
 			const cachedResponse = await getCachedResponse(request);
 			if (!cachedResponse?.body) {
 				return false;
@@ -68,9 +77,11 @@ async function handleNetworkResponse(request: Request, response: Response) {
 			return hasChanged;
 		})();
 
-		if (shouldClearCache) {
-			await clearCache();
-		}
 		addToCache(request, responseToCache);
+		if (shouldRefreshCache) {
+			await refreshCache({
+				exceptions: [request],
+			});
+		}
 	}
 }
