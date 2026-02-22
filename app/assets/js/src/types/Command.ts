2c2feb7ed3b70a1483d2b4496c995d33b955887f
Brooke Hart <brooke+github@curly.kiwi>
2026-02-22 16:08:24 +1300

    Add "Go to task" command

--- app/assets/js/src/types/Command.ts
+++ app/assets/js/src/types/Command.ts
@@ -3,6 +3,7 @@ import type { EnumTypeOf } from 'utils';
 export const Command = {
 	DAY_ADD_NEW: 'day-add-new',
 	TASK_ADD_NEW: 'task-add-new',
+	TASK_GO_TO_EXISTING: 'task-go-to-existing',
 	DATA_SAVE: 'data-save',
 	DATA_EXPORT: 'data-export',
 	DATA_IMPORT: 'data-import',
@@ -16,6 +17,7 @@ declare module 'registers/commands' {
 	interface CommandsList {
 		[Command.DAY_ADD_NEW]: [dayName: string];
 		[Command.TASK_ADD_NEW]: [dayName: string];
+		[Command.TASK_GO_TO_EXISTING]: [taskId: number];
 		[Command.DATA_SAVE]: [];
 		[Command.DATA_EXPORT]: [];
 		[Command.DATA_IMPORT]: [];
