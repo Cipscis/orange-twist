0968fac1405eeeac4a3eeea8cd99e6e51ce90c65
Brooke Hart <brooke+github@curly.kiwi>
2026-03-24 12:39:25 +1300

    Refactor load to separate Register logic

--- app/assets/js/src/data/shared/index.ts
+++ app/assets/js/src/data/shared/index.ts
@@ -2,4 +2,6 @@ export { clear } from './clear';
 export { exportData } from './exportData/exportData';
 export { getTaskStatusForDay } from './getTaskStatusForDay';
 export { importData } from './importData/importData';
+export { loadData } from './loadData';
+export type { DataSource } from './types/DataSource';
 export { StorageKey } from './StorageKey';
