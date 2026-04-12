5490bfbb68a6a9af9fc513521a58ccf6b1ade064
Brooke Hart <brooke+github@curly.kiwi>
2026-04-12 15:23:24 +1200

    Use Zod schemas for update data types

--- app/assets/js/src/data/shared/types/LegacyExportDataVersions/LegacyExportDataV1_0_0.ts
+++ app/assets/js/src/data/shared/types/LegacyExportDataVersions/LegacyExportDataV1_0_0.ts
@@ -1,56 +1,91 @@
-import type { LegacyStatusName } from './LegacyStatusName';
+import * as z from 'zod/mini';
 
-export type LegacyExportDataV1_0_0 = {
-	// TODO: Actual export data won't have this
-	schemaVersion: '1.0.0';
-	days: [string, {
-		name: string;
-		note: string;
-		tasks: readonly number[];
-	}][];
-	tasks: [number,
-		| {
-			id: number;
-			name: string;
-			status: LegacyStatusName;
-		}
-		| {
-			id: number;
-			name: string;
-			status: LegacyStatusName;
-			note: string;
-		}
-		| {
-			id: number;
-			name: string;
-			status: LegacyStatusName;
-			note: string;
-			sortIndex: number;
-		}
-	][];
-	dayTasks: [`${string}_${number}`,
-		| {
-			dayName: string;
-			taskId: number;
-			status: LegacyStatusName;
-			note: string;
-		}
-		| {
-			status: LegacyStatusName;
-			note: string;
-			summary: string;
-		}
-	][];
-	templates?: [number, {
-		id: number;
-		name: string;
-		template: string;
-		sortIndex: number;
-	}][];
-	images?: [
-		string,
-		// Blobs get serialised to a Data URL for export data
-		| string
-		| Blob
-	][];
-};
+import { legacyStatusNameSchema } from './LegacyStatusName';
+import { isZodSchemaType } from 'utils';
+
+export const legacyExportDataSchemaV1_0_0 = z.object({
+	schemaVersion: z.literal('1.0.0'),
+	days: z.array(
+		z.tuple([
+			z.string(),
+			z.object({
+				name: z.string(),
+				note: z.string(),
+				tasks: z.readonly(z.array(z.number())),
+			}),
+		]),
+	),
+	tasks: z.array(
+		z.tuple([
+			z.number(),
+			z.union([
+				z.object({
+					id: z.number(),
+					name: z.string(),
+					status: legacyStatusNameSchema,
+				}),
+				z.object({
+					id: z.number(),
+					name: z.string(),
+					status: legacyStatusNameSchema,
+					note: z.string(),
+				}),
+				z.object({
+					id: z.number(),
+					name: z.string(),
+					status: legacyStatusNameSchema,
+					note: z.string(),
+					sortIndex: z.number(),
+				}),
+			]),
+		]),
+	),
+	dayTasks: z.array(
+		z.tuple([
+			z.stringFormat('dayTask identifier', (val) => /\d{4}-\d{2}-\d{2}_\d+/.test(val)),
+			z.union([
+				z.object({
+					dayName: z.string(),
+					taskId: z.number(),
+					status: legacyStatusNameSchema,
+					note: z.string(),
+				}),
+				z.object({
+					status: legacyStatusNameSchema,
+					note: z.string(),
+					summary: z.string(),
+				}),
+			]),
+		]),
+	),
+	templates: z.optional(
+		z.array(
+			z.tuple([
+				z.number(),
+				z.object({
+					id: z.number(),
+					name: z.string(),
+					template: z.string(),
+					sortIndex: z.number(),
+				}),
+			]),
+		),
+	),
+	images: z.optional(
+		z.array(
+			z.tuple([
+				z.string(),
+				z.union([
+					z.url({
+						protocol: /^data$/,
+					}),
+					z.instanceof(Blob),
+				]),
+			]),
+		),
+	),
+});
+
+export type LegacyExportDataV1_0_0 = z.infer<typeof legacyExportDataSchemaV1_0_0>;
+
+export const isLegacyExportDataV1_0_0 = isZodSchemaType(legacyExportDataSchemaV1_0_0);
