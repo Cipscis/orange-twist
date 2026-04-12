e9fdb252064c7d085e30d4c7e2bc0c46037f153a
Brooke Hart <brooke+github@curly.kiwi>
2026-04-12 14:06:46 +1200

    Refactored update code from schema version 1.0.0 -> 2.0.0

---
+++ app/assets/js/src/data/shared/updateData/updateDataV1_0_0.ts
@@ -0,0 +1,261 @@
+import { decodeDayTaskKey } from 'data/dayTasks/util';
+import type { LegacyExportData } from '../types';
+import type {
+	LegacyExportDataByVersion,
+	LegacyStatusName,
+} from '../types/LegacyExportDataVersions';
+
+/**
+ * Update a {@linkcode LegacyExportData} from schema version `1.0.0` to schema version `2.0.0`.
+ */
+export async function updateDataV1_0_0(
+	legacyData: LegacyExportDataByVersion<'1.0.0'>
+): Promise<LegacyExportDataByVersion<'2.0.0'>> {
+	const day = collectDayData(legacyData);
+	const status = collectStatusData(legacyData);
+	const task = collectTaskData(legacyData, status);
+	const day_task = collectDayTaskData(legacyData, day, status);
+	const template = collectTemplateData(legacyData);
+	const image = await collectImageData(legacyData);
+
+	const updatedData: Extract<LegacyExportData, { schemaVersion: '2.0.0'; }> = {
+		schemaVersion: '2.0.0',
+
+		day,
+		task,
+		day_task,
+		status,
+		template,
+		image,
+	};
+
+	return updatedData;
+}
+
+/**
+ * Collect day data suitable for schema version `2.0.0` from schema version `1.0.0` data.
+ */
+function collectDayData(
+	legacyData: Readonly<LegacyExportDataByVersion<'1.0.0'>>
+): LegacyExportDataByVersion<'2.0.0'>['day'] {
+	return legacyData.days.map(([dayName, legacyDay], id) => {
+		const [year, month, day] = getDayNameParts(dayName);
+
+		return {
+			id,
+			note: legacyDay.note,
+			year,
+			month,
+			day,
+		};
+	});
+}
+
+/**
+ * Collect status data suitable for schema version `2.0.0` from schema version `1.0.0` data.
+ */
+function collectStatusData(
+	legacyData: Readonly<LegacyExportDataByVersion<'1.0.0'>>
+): LegacyExportDataByVersion<'2.0.0'>['status'] {
+	// Collect statuses in data set, starting with defaults
+	const statusSet = new Set<string>([
+		'todo',
+		'in-progress',
+		'completed',
+
+		'investigating',
+		'in-review',
+		'ready-to-test',
+		'paused',
+		'approved-to-deploy',
+		'will-not-do',
+	] satisfies LegacyStatusName[]);
+	for (const [, { status }] of legacyData.tasks) {
+		statusSet.add(status);
+	}
+	for (const [, { status }] of legacyData.dayTasks) {
+		statusSet.add(status);
+	}
+
+	return Array.from(statusSet)
+		.map((name, id) => ({
+			id,
+			name,
+			isComplete: ['completed', 'will-not-do'].includes(name),
+		}));
+}
+
+/**
+ * Collect task data suitable for schema version `2.0.0` from schema version `1.0.0` data and already collected status data for schema version `2.0.0`.
+ */
+function collectTaskData(
+	legacyData: Readonly<LegacyExportDataByVersion<'1.0.0'>>,
+	statusData: Readonly<LegacyExportDataByVersion<'2.0.0'>['status']>,
+): LegacyExportDataByVersion<'2.0.0'>['task'] {
+	return legacyData.tasks.map(([id, taskInfo]) => ({
+		id,
+		name: taskInfo.name,
+		note: ('note' in taskInfo) ? taskInfo.note : '',
+		sortIndex: ('sortIndex' in taskInfo) ? taskInfo.sortIndex : null,
+		status: getStatusIdByName(taskInfo.status, statusData),
+	}));
+}
+
+/**
+ * Collect day task data suitable for schema version `2.0.0` from schema version `1.0.0` data and already collected day and status data for schema version `2.0.0`.
+ *
+ * May insert new day information if day tasks are found for days that don't exist in dayData.
+ */
+function collectDayTaskData(
+	legacyData: Readonly<LegacyExportDataByVersion<'1.0.0'>>,
+	dayData: LegacyExportDataByVersion<'2.0.0'>['day'],
+	statusData: Readonly<LegacyExportDataByVersion<'2.0.0'>['status']>
+): LegacyExportDataByVersion<'2.0.0'>['day_task'] {
+	return legacyData.dayTasks.map(
+		([dayTaskKey, dayTaskInfo], id) => {
+			const { dayName, taskId } = decodeDayTaskKey(dayTaskKey);
+
+			const dayId = (() => {
+				const dayId = getDayIdByName(dayName, dayData);
+				if (typeof dayId === 'number') {
+					return dayId;
+				}
+
+				// Otherwise, construct a new day and insert it, then use that id
+				const nextId = Math.max(...dayData.map(({ id }) => id));
+				const [year, month, dayNumber] = getDayNameParts(dayName);
+				dayData.push({
+					id: nextId,
+					year,
+					month,
+					day: dayNumber,
+					note: '',
+				});
+				return nextId;
+			})();
+
+			const sortIndex = (() => {
+				const legacyDayInfo = legacyData.days.find(([name]) => name === dayName)?.[1];
+				if (!legacyDayInfo) {
+					return null;
+				}
+
+				const dayTaskIndex = legacyDayInfo.tasks.indexOf(taskId);
+				if (dayTaskIndex === -1) {
+					return null;
+				}
+
+				return dayTaskIndex;
+			})();
+
+			return {
+				id,
+				day: dayId,
+				task: taskId,
+				note: dayTaskInfo.note,
+				status: getStatusIdByName(dayTaskInfo.status, statusData),
+				summary: ('summary' in dayTaskInfo) ? dayTaskInfo.summary : '',
+				sortIndex,
+			};
+		}
+	);
+}
+
+/**
+ * Collect template data suitable for schema version `2.0.0` from schema version `1.0.0` data.
+ */
+function collectTemplateData(
+	legacyData: Readonly<LegacyExportDataByVersion<'1.0.0'>>
+): LegacyExportDataByVersion<'2.0.0'>['template'] {
+	if (!legacyData.templates) {
+		return [];
+	}
+
+	return legacyData.templates.map(([id, templateInfo]) => ({
+		id,
+		name: templateInfo.name,
+		template: templateInfo.template,
+		sortIndex: templateInfo.sortIndex,
+	}));
+}
+
+/**
+ * Collect image data suiable for schema version `2.0.0` from schema version `1.0.0` data.
+ *
+ * @throws {Error} if an unsafe URL string is encountered. Only `data:` protocol URLs are permitted.
+ */
+async function collectImageData(
+	legacyData: Readonly<LegacyExportDataByVersion<'1.0.0'>>
+): Promise<LegacyExportDataByVersion<'2.0.0'>['image']> {
+	return await Promise.all(
+		legacyData.images?.map(async ([hash, data], id) => {
+			const file = await (async () => {
+				if (data instanceof Blob) {
+					return data;
+				}
+
+				const url = new URL(data);
+				if (url.protocol !== 'data:') {
+					throw new Error('Error during data update: Unsafe URL encountered');
+				}
+
+				const imageResponse = await fetch(url);
+				const imageBlob = await imageResponse.blob();
+
+				return imageBlob;
+			})();
+
+			return {
+				id,
+				hash,
+				file,
+			};
+		}) ?? []
+	);
+}
+
+/**
+ * Convert a legacy day name into numeric parts. For example, `'2026-04-12'` becomes `[2026, 4, 12]`.
+ */
+function getDayNameParts(dayName: string): [number, number, number] {
+	const parts = dayName.split('-');
+	const year = Number(parts[0]);
+	const month = Number(parts[1]);
+	const day = Number(parts[2]);
+
+	return [year, month, day];
+}
+
+/**
+ * Within schema version `2.0.0` day data, look up the ID of a day based on its legacy day name.
+ *
+ * If no day is found, returns `null`.
+ */
+function getDayIdByName(
+	dayName: string,
+	dayData: Readonly<LegacyExportDataByVersion<'2.0.0'>['day']>,
+): number | null {
+	const [yearToMatch, monthToMatch, dayToMatch] = getDayNameParts(dayName);
+
+	const matchingDay = dayData.find(({ year, month, day }) => {
+		return year === yearToMatch &&
+		month === monthToMatch &&
+		day === dayToMatch;
+	});
+
+	return matchingDay?.id ?? null;
+}
+
+/**
+ * Within schema version `2.0.0` status data, look up the ID of a status based on its legacy name.
+ *
+ * If no status is found, returns the ID of the first status by default (`0`).
+ */
+function getStatusIdByName(
+	statusName: LegacyStatusName,
+	statusData: Readonly<LegacyExportDataByVersion<'2.0.0'>['status']>,
+): number {
+	const matchingStatus = statusData.find(({ name }) => name === statusName);
+
+	return matchingStatus?.id ?? 0;
+}
