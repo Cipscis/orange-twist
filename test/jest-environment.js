da552c90734f6a06e183c6742efff7d3c39dbea3
Brooke Hart <brooke+github@curly.kiwi>
2026-04-12 13:37:11 +1200

    Created test environment with Fetch API

---
+++ test/jest-environment.js
@@ -0,0 +1,17 @@
+/**
+ * @file This file is plain JavaScript because my attempts to make it TypeScript have run into issues with Jest forgetting that it's operating in an ES Module context, despite all the configuration making it clear that this project has nothing to do with CommonJS.
+ */
+
+import JSDOMEnvironment from 'jest-environment-jsdom';
+
+/**
+ * A modified JSDOM environment that includes the Node implementation of the Fetch API.
+ */
+export default class JSDOMEnvironmentWithFetch extends JSDOMEnvironment {
+	constructor(...args) {
+		super(...args);
+		this.global.fetch = fetch;
+		this.global.Request = Request;
+		this.global.Response = Response;
+	}
+}
