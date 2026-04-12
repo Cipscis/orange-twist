4c4983277d678c26d83d2a1d3256dfb72c6644b6
Brooke Hart <brooke+github@curly.kiwi>
2026-04-12 15:23:24 +1200

    Use Zod schemas for update data types

--- app/assets/js/src/data/shared/types/LegacyExportDataVersions/LegacyStatusName.ts
+++ app/assets/js/src/data/shared/types/LegacyExportDataVersions/LegacyStatusName.ts
@@ -1,10 +1,15 @@
-export type LegacyStatusName =
-	| 'todo'
-	| 'in-progress'
-	| 'completed'
-	| 'investigating'
-	| 'in-review'
-	| 'ready-to-test'
-	| 'paused'
-	| 'approved-to-deploy'
-	| 'will-not-do';
+import * as z from 'zod/mini';
+
+export const legacyStatusNameSchema = z.union([
+	z.literal('todo'),
+	z.literal('in-progress'),
+	z.literal('completed'),
+	z.literal('investigating'),
+	z.literal('in-review'),
+	z.literal('ready-to-test'),
+	z.literal('paused'),
+	z.literal('approved-to-deploy'),
+	z.literal('will-not-do'),
+]);
+
+export type LegacyStatusName = z.infer<typeof legacyStatusNameSchema>;
