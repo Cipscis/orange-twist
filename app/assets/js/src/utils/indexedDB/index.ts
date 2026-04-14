ffa3f48b16ae9c33200bbd02d6cc1358d6cbd52b
Brooke Hart <brooke+github@curly.kiwi>
2026-04-14 12:50:41 +1200

    Export `getIdbRequestPromise`

--- app/assets/js/src/utils/indexedDB/index.ts
+++ app/assets/js/src/utils/indexedDB/index.ts
@@ -1,4 +1,5 @@
 export { doDatabaseTransaction } from './doDatabaseTransaction';
 export { getDatabase } from './getDatabase';
 export { getEntries } from './getEntries';
+export { getIdbRequestPromise } from './getIdbRequestPromise';
 export { ObjectStoreName } from './ObjectStoreName';
