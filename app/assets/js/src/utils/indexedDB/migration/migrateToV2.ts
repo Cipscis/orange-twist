d30838fbf3588eb4fc44110a68509f5cf4bd0851
Brooke Hart <brooke+github@curly.kiwi>
2026-04-14 13:05:26 +1200

    Create new object stores for database v2

--- app/assets/js/src/utils/indexedDB/migration/migrateToV2.ts
+++ app/assets/js/src/utils/indexedDB/migration/migrateToV2.ts
@@ -1,7 +1,18 @@
+import { ObjectStoreName } from '../ObjectStoreName';
+
 /**
  * Migrate the Orange Twist database from v1 to v2.
  */
 export function migrateToV2(db: IDBDatabase): void {
 	console.log('updating database to v2...');
+	db.deleteObjectStore(ObjectStoreName.DATA);
+	db.deleteObjectStore(ObjectStoreName.IMAGES);
+
+	db.createObjectStore(ObjectStoreName.DAY);
+	db.createObjectStore(ObjectStoreName.TASK);
+	db.createObjectStore(ObjectStoreName.DAY_TASK);
+	db.createObjectStore(ObjectStoreName.STATUS);
+	db.createObjectStore(ObjectStoreName.TEMPLATE);
+	db.createObjectStore(ObjectStoreName.IMAGE);
 	console.log('updated database v2');
 }
