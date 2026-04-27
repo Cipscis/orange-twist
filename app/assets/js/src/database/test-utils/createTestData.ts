6f788d0920376b2fc73353515797c94df578a4b7
Brooke Hart <brooke+github@curly.kiwi>
2026-04-27 14:54:10 +1200

    Added tests for getDaysV1

---
+++ app/assets/js/src/database/test-utils/createTestData.ts
@@ -0,0 +1,96 @@
+import { clearDatabase } from './clearDatabase';
+
+import type { DatabaseData } from 'data/shared/types';
+import { ObjectStoreName } from 'database/metadata';
+import { getDatabase, getIdbRequestPromise } from 'utils/indexedDB';
+
+/**
+ * **Important!** For use within tests only.
+ *
+ * Construct an Orange Twist database v2 containing test data.
+ */
+export async function createTestData(): Promise<void> {
+	// Start with a fresh database
+	await clearDatabase();
+	const database = await getDatabase(true);
+
+	const transaction = database.transaction([
+		ObjectStoreName.DAY,
+		ObjectStoreName.TASK,
+		ObjectStoreName.DAY_TASK,
+		ObjectStoreName.STATUS,
+		ObjectStoreName.TEMPLATE,
+		ObjectStoreName.IMAGE,
+	], 'readwrite');
+
+	const dayOS = transaction.objectStore(ObjectStoreName.DAY);
+	const taskOS = transaction.objectStore(ObjectStoreName.TASK);
+	const dayTaskOS = transaction.objectStore(ObjectStoreName.DAY_TASK);
+
+	const requests: IDBRequest[] = [];
+
+	// TODO: Once it exists, use the database API for setting data
+
+	// Insert test days
+	requests.push(dayOS.put({
+		id: 0,
+		year: 2026,
+		month: 4,
+		day: 26,
+		note: 'Test note',
+	} satisfies DatabaseData[typeof ObjectStoreName.DAY][number]));
+
+	requests.push(dayOS.put({
+		id: 1,
+		year: 2026,
+		month: 4,
+		day: 27,
+		note: 'Test note 2',
+	} satisfies DatabaseData[typeof ObjectStoreName.DAY][number]));
+
+	// Insert test tasks
+	requests.push(taskOS.put({
+		id: 0,
+		name: 'Test task 0',
+		note: 'Test task 0 note',
+		status: 0,
+		sortIndex: 0,
+	} satisfies DatabaseData[typeof ObjectStoreName.TASK][number]));
+
+	requests.push(taskOS.put({
+		id: 1,
+		name: 'Test task 1',
+		note: 'Test task 1 note',
+		status: 1,
+		sortIndex: 1,
+	} satisfies DatabaseData[typeof ObjectStoreName.TASK][number]));
+
+	// Insert test day tasks
+	requests.push(dayTaskOS.put({
+		id: 0,
+		day: 0,
+		task: 0,
+		note: 'Note for task 0 day 0',
+		summary: 'Summary for task 0 day 0',
+		status: 1,
+		sortIndex: 0,
+	} satisfies DatabaseData[typeof ObjectStoreName.DAY_TASK][number]));
+
+	requests.push(dayTaskOS.put({
+		id: 1,
+		day: 0,
+		task: 1,
+		note: 'Note for task 1 day 0',
+		summary: 'Summary for task 1 day 0',
+		status: 1,
+		sortIndex: 1,
+	} satisfies DatabaseData[typeof ObjectStoreName.DAY_TASK][number]));
+
+	// TODO: Insert test templates
+
+	// TODO: Insert test images
+
+	await Promise.all(
+		requests.map((request) => getIdbRequestPromise(request))
+	);
+}
