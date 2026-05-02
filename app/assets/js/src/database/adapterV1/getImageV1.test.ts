f4b9436f4161a7a7319fc7e8b35bf9b8580a87ce
Brooke Hart <brooke+github@curly.kiwi>
2026-05-03 11:10:00 +1200

    Updated getImage function

---
+++ app/assets/js/src/database/adapterV1/getImageV1.test.ts
@@ -0,0 +1,28 @@
+import {
+	beforeAll,
+	describe,
+	expect,
+	test,
+} from '@jest/globals';
+
+import { createTestData } from 'database/test-utils';
+
+import { getImageV1 } from './getImageV1';
+
+describe('getImageV1', () => {
+	beforeAll(async () => {
+		await createTestData();
+	});
+
+	test('returns a Promise that resolves to an image v1', async () => {
+		const image = await getImageV1('test-hash');
+
+		expect(image).toBeInstanceOf(Blob);
+	});
+
+	test('resolves to null if no image exists at that hash', async () => {
+		const noImage = await getImageV1('no-image');
+
+		expect(noImage).toBeNull();
+	});
+});
---
+++ app/assets/js/src/database/getImage.ts
@@ -0,0 +1,16 @@
+import { getDatabase } from 'utils/indexedDB';
+
+import { ObjectStoreName } from './metadata';
+import type { DatabaseData } from './types';
+import { getImageByHashInternal } from './internal';
+
+export async function getImage(hash: string): Promise<
+	| DatabaseData[typeof ObjectStoreName.IMAGE][number]
+	| null
+> {
+	const db = await getDatabase();
+	const transaction = db.transaction(ObjectStoreName.IMAGE);
+	const imageOS = transaction.objectStore(ObjectStoreName.IMAGE);
+
+	return await getImageByHashInternal(imageOS, hash);
+}
