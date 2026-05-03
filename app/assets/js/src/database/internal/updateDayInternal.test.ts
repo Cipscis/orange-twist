5a0376c27279df48489ebda8ac09c1f51dfa3e63
Brooke Hart <brooke+github@curly.kiwi>
2026-05-03 13:13:38 +1200

    Added internal db function for updating an existing day

---
+++ app/assets/js/src/database/internal/updateDayInternal.test.ts
@@ -0,0 +1,54 @@
+import {
+	beforeAll,
+	beforeEach,
+	describe,
+	expect,
+	test,
+} from '@jest/globals';
+
+import { ObjectStoreName } from '../metadata';
+import { createTestData } from '../test-utils';
+import { getDatabase } from '../utils';
+
+import { updateDayInternal } from './updateDayInternal';
+import { getDayByDateInternal } from './getDayByDateInternal';
+
+describe('updateDayInternal', () => {
+	let dayOS: IDBObjectStore;
+
+	beforeEach(async () => {
+		await createTestData();
+
+		const db = await getDatabase();
+		const transaction = db.transaction(ObjectStoreName.DAY, 'readwrite');
+		dayOS = transaction.objectStore(ObjectStoreName.DAY);
+	});
+
+	test('updates a specified day', async () => {
+		const result = await updateDayInternal(dayOS, {
+			year: 2026,
+			month: 4,
+			day: 27,
+			note: 'Updated note',
+		});
+
+		expect(result).toBeUndefined();
+
+		const updatedDay = await getDayByDateInternal(dayOS, {
+			year: 2026,
+			month: 4,
+			day: 27,
+		});
+
+		expect(updatedDay?.note).toBe('Updated note');
+	});
+
+	test('throws an error if the day doesn\'t exist', async () => {
+		await expect(updateDayInternal(dayOS, {
+			year: 2020,
+			month: 1,
+			day: 1,
+			note: 'Failed update',
+		})).rejects.toBeInstanceOf(Error);
+	});
+});
