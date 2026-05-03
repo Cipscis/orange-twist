f47d739db9dcce83577838f318ff5d9db0abd1a9
Brooke Hart <brooke+github@curly.kiwi>
2026-05-03 12:07:32 +1200

    Added internal db function for adding a day

---
+++ app/assets/js/src/database/internal/addDayInternal.test.ts
@@ -0,0 +1,70 @@
+import {
+	beforeEach,
+	describe,
+	expect,
+	test,
+} from '@jest/globals';
+
+import { getDatabase } from '../utils';
+import { ObjectStoreName } from '../metadata';
+import { clearDatabase } from '../test-utils';
+
+import { getDayInternal } from './getDayInternal';
+
+import { addDayInternal } from './addDayInternal';
+
+describe('addDayInternal', () => {
+	beforeEach(() => clearDatabase());
+
+	test('inserts a new day into the database, and returns its ID', async () => {
+		const db = await getDatabase(true);
+		const writeTransaction = db.transaction(ObjectStoreName.DAY, 'readwrite');
+		const writeDayOS = writeTransaction.objectStore(ObjectStoreName.DAY);
+
+		const writeResult = await addDayInternal(writeDayOS, {
+			year: 2026,
+			month: 5,
+			day: 3,
+			note: 'Test note',
+		});
+
+		expect(writeResult).toBe(1);
+
+		const readTransaction = db.transaction(ObjectStoreName.DAY, 'readonly');
+		const readDayOS = readTransaction.objectStore(ObjectStoreName.DAY);
+
+		const readResult = await getDayInternal(readDayOS, 1);
+
+		expect(readResult).toEqual({
+			id: 1,
+			year: 2026,
+			month: 5,
+			day: 3,
+			note: 'Test note',
+		});
+	});
+
+	test('throws an error if a day already exists with that date', async () => {
+		const db = await getDatabase(true);
+		const transaction = db.transaction(ObjectStoreName.DAY, 'readwrite');
+		const dayOS = transaction.objectStore(ObjectStoreName.DAY);
+
+		// Add a day first
+		await addDayInternal(dayOS, {
+			year: 2026,
+			month: 5,
+			day: 3,
+			note: 'Test note',
+		});
+
+		// Then try adding it again
+		await expect(
+			() => addDayInternal(dayOS, {
+				year: 2026,
+				month: 5,
+				day: 3,
+				note: 'Test note',
+			})
+		).rejects.toBeInstanceOf(Error);
+	});
+});
