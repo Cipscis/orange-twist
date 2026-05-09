da637a9c9ed076833aade0dd8fbd1e52d17aca4a
Brooke Hart <brooke+github@curly.kiwi>
2026-05-09 21:39:53 +1200

    Added test suite for getIdbRequestPromise

--- app/assets/js/src/utils/indexedDB/getIdbRequestPromise.ts
+++ app/assets/js/src/utils/indexedDB/getIdbRequestPromise.ts
@@ -3,6 +3,15 @@
  */
 export function getIdbRequestPromise<T>(request: IDBRequest<T>): Promise<T> {
 	return new Promise((resolve, reject) => {
+		if (request.readyState === 'done') {
+			if (request.error) {
+				reject(request.error);
+			} else {
+				resolve(request.result);
+			}
+			return;
+		}
+
 		request.addEventListener('success', () => resolve(request.result));
 		request.addEventListener('error', () => reject(request.error!));
 	});
