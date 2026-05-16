850f0490cddf6180a0ccc5324ff77e3f028f9e49
Brooke Hart <brooke+github@curly.kiwi>
2026-05-16 14:06:55 +1200

    Refactored getTemplatesInternal to take an IDBTransaction

--- app/assets/js/src/database/internal/getTemplatesInternal.ts
+++ app/assets/js/src/database/internal/getTemplatesInternal.ts
@@ -1,11 +1,20 @@
 import { getIdbRequestPromise, sortBySortIndex } from 'utils';
 
-import type { ObjectStoreName } from '../metadata';
+import { ObjectStoreName } from '../metadata';
 import type { DatabaseData } from '../types';
 
-export async function getTemplatesInternal(templateOS: IDBObjectStore): Promise<
+/**
+ * Takes an existing {@linkcode IDBTransaction} and adds a request to get all tempaltes.
+ *
+ * @param transaction An {@linkcode IDBTransaction} with read permission and access to the {@linkcode ObjectStoreName.TEMPLATE} object store.
+ *
+ * @returns A {@linkcode Promise} that resolves to an array containing all templates, sorted according to their `sortIndex` property.
+ */
+export async function getTemplatesInternal(transaction: IDBTransaction): Promise<
 	DatabaseData[typeof ObjectStoreName.TEMPLATE][number][]
 > {
+	const templateOS = transaction.objectStore(ObjectStoreName.TEMPLATE);
+
 	// TODO: Find a type-safe way to do this
 	const request = templateOS.getAll() as IDBRequest<
 		DatabaseData[typeof ObjectStoreName.TEMPLATE][number][]
