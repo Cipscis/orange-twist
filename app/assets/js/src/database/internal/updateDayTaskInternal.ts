1e2c5d367101b306103e68c83610d1178c908998
Brooke Hart <brooke+github@curly.kiwi>
2026-05-17 12:35:35 +1200

    Return day task ID from updateDayTaskInternal

--- app/assets/js/src/database/internal/updateDayTaskInternal.ts
+++ app/assets/js/src/database/internal/updateDayTaskInternal.ts
@@ -15,6 +15,8 @@ import { getStatusInternal } from './getStatusInternal';
  * @param transaction An {@linkcode IDBTransaction} with write permission and access to the {@linkcode ObjectStoreName.DAY_TASK} and {@linkcode ObjectStoreName.STATUS} object stores.
  * @param dayTask An object specifiny which day task to update by the IDs of its day and task, and providing any values that should be updated.
  *
+ * @returns a {@linkcode Promise} that resolves with the ID of the updated day task, once the update is complete.
+ *
  * @throws Error if no day task exists with the specified day and task IDs.
  * @throws Error if no status exists with the specified status ID.
  */
@@ -24,15 +26,17 @@ export async function updateDayTaskInternal(
 		Omit<DatabaseData[typeof ObjectStoreName.DAY_TASK][number], 'id'>,
 		'day' | 'task'
 	>
-): Promise<void> {
+): Promise<number> {
 	const dayTaskOS = transaction.objectStore(ObjectStoreName.DAY_TASK);
 	const dayTaskByDayAndTask = dayTaskOS.index(IndexName.DAY_TASK_DAY_TASK);
 
 	const requests: Promise<IDBValidKey>[] = [];
+	let dayTaskId: number | undefined;
 
 	for await (const dayTaskCursor of getIterableCursor(dayTaskByDayAndTask, [dayTask.day, dayTask.task])) {
 		// TODO: Find a type-safe way to do this
-		const dayTaskId = (dayTaskCursor.value as DatabaseData[typeof ObjectStoreName.DAY_TASK][number]).id;
+		const dayTaskCursorValue = (dayTaskCursor.value as DatabaseData[typeof ObjectStoreName.DAY_TASK][number]);
+		dayTaskId = dayTaskCursorValue.id;
 
 		if (typeof dayTask.status === 'number') {
 			const status = await getStatusInternal(transaction, dayTask.status);
@@ -44,16 +48,18 @@ export async function updateDayTaskInternal(
 		requests.push(
 			getIdbRequestPromise(
 				dayTaskCursor.update({
-					...dayTaskCursor.value,
+					...dayTaskCursorValue,
 					...dayTask,
 				})
 			)
 		);
 	}
 
-	if (requests.length === 0) {
+	if (typeof dayTaskId === 'undefined') {
 		throw new Error(`Failed to update day task with day ID ${dayTask.day} and task ID ${dayTask.task} - No such day task exists.`);
 	}
 
 	await Promise.all(requests);
+
+	return dayTaskId;
 }
