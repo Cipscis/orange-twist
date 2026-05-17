3968fb0dbc2b2398f38420f983433f5d6fd80b7c
Brooke Hart <brooke+github@curly.kiwi>
2026-05-17 13:10:46 +1200

    Updated comment about return value

--- app/assets/js/src/database/internal/addTaskInternal.ts
+++ app/assets/js/src/database/internal/addTaskInternal.ts
@@ -12,7 +12,7 @@ import { getStatusInternal } from './getStatusInternal';
  * @param transaction An {@linkcode IDBTransaction} with write permission and access to the {@linkcode ObjectStoreName.TASK} and {@linkcode ObjectStoreName.STATUS} object stores.
  * @param task The day object to insert. Any missing properties will be filled with sensible defaults.
  *
- * @returns A {@linkcode Promise} that resolves when the task has been added.
+ * @returns A {@linkcode Promise} that resolves with the task's ID when id has been added.
  *
  * @throws Error if a task already exists with the specified ID.
  * @throws Error if the task is linked to a status ID that does not exist.
