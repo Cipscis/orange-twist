fef91f5c5ffcfa44319712bae281e93a6cfa0fc7
Brooke Hart <brooke+github@curly.kiwi>
2026-05-17 21:20:31 +1200

    Wrote test for updating data from Orange Twist 1.5.0 or lower

--- app/assets/js/src/images/getAllImages.ts
+++ app/assets/js/src/images/getAllImages.ts
@@ -1,23 +1,8 @@
-import { getEntries } from 'utils';
-
 import { adapterV1 } from 'database';
-import { getDatabase, getDatabaseVersion } from 'database/utils';
-import { ObjectStoreName } from 'database/metadata';
 
 /**
  * Retrieve all images saved in the image object store, as [key, value] tuples.
  */
 export async function getAllImages(): Promise<(readonly [hash: string, image: Blob])[]> {
-	// TODO: Get rid of this database v1 handling
-	const dbVersion = await getDatabaseVersion();
-	if (dbVersion === 1 || dbVersion === null) {
-		const db = await getDatabase();
-		const transaction = db.transaction(ObjectStoreName.IMAGES, 'readonly');
-		const imageOS = transaction.objectStore(ObjectStoreName.IMAGES);
-
-		const entries = await Array.fromAsync(getEntries(imageOS));
-		return entries as [string, Blob][];
-	}
-
 	return await adapterV1.getImages();
 }
