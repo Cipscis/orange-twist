0e06b99470cb5b612e35d3e8f4304379f57e0558
Brooke Hart <brooke+github@curly.kiwi>
2026-07-04 13:01:49 +1200

    Implemented setImagesV1

--- app/assets/js/src/images/setAllImages.ts
+++ app/assets/js/src/images/setAllImages.ts
@@ -1,7 +1,4 @@
-import { getEntries } from 'utils';
-
-import { ObjectStoreName } from 'database/metadata';
-import { getDatabase, getDatabaseVersion } from 'database/utils';
+import { adapterV1 } from 'database';
 
 /**
  * First, erases all existing images in IndexedDB. Then, adds all
@@ -10,31 +7,5 @@ import { getDatabase, getDatabaseVersion } from 'database/utils';
 export async function setAllImages(
 	images: readonly (readonly [string, Blob])[]
 ): Promise<void> {
-	// Handle indexedDB not existing in JSDom for tests
-	if (!self.indexedDB) {
-		return;
-	}
-
-	// TODO: Get rid of this database v1 handling
-	const dbVersion = await getDatabaseVersion();
-	if (dbVersion === 1 || dbVersion === null) {
-		// In the same database transaction, delete all images then set all images
-		const db = await getDatabase();
-		const transaction = db.transaction(ObjectStoreName.IMAGES, 'readwrite');
-		const objectStore = transaction.objectStore(ObjectStoreName.IMAGES);
-
-		const entries = getEntries(objectStore);
-		for await (const [key] of entries) {
-			objectStore.delete(key);
-		}
-
-		for (const [key, imageBlob] of images) {
-			objectStore.add(imageBlob, key);
-		}
-
-		return;
-	}
-
-	// TODO: Implement v2 handling
-	throw new Error('Saving image not implemented');
+	await adapterV1.setImages(images);
 }
