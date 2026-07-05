da889e27425df43b39949c0d314ceb59bdb55548
Brooke Hart <brooke+github@curly.kiwi>
2026-07-05 14:25:16 +1200

    Moved getDayNameParts to database/utils

--- app/assets/js/src/database/migration/updateDataV1_0_0.ts
+++ app/assets/js/src/database/migration/updateDataV1_0_0.ts
@@ -7,6 +7,7 @@ import type {
 	LegacyExportDataByVersion,
 	LegacyStatusName,
 } from '../types';
+import { getDayNameParts } from '../utils';
 import { collectStatusData } from './collectStatusData';
 
 /**
@@ -195,18 +196,6 @@ async function collectImageData(
 	);
 
 	return images;
-}
-
-/**
- * Convert a legacy day name into numeric parts. For example, `'2026-04-12'` becomes `[2026, 4, 12]`.
- */
-function getDayNameParts(dayName: string): [number, number, number] {
-	const parts = dayName.split('-');
-	const year = Number(parts[0]);
-	const month = Number(parts[1]);
-	const day = Number(parts[2]);
-
-	return [year, month, day];
 }
 
 /**
--- app/assets/js/src/database/utils/index.ts
+++ app/assets/js/src/database/utils/index.ts
@@ -1,3 +1,4 @@
 export { doDatabaseTransaction } from './doDatabaseTransaction';
 export { getDatabase } from './getDatabase';
 export { getDatabaseVersion } from './getDatabaseVersion';
+export { getDayNameParts } from './getDayNameParts';
