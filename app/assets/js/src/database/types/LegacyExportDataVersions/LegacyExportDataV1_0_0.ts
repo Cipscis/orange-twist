565ec3e4e61574478ec7ff91e3d7586d53567ed0
Brooke Hart <brooke+github@curly.kiwi>
2026-07-07 21:39:06 +1200

    Allow empty data from schema v1

--- app/assets/js/src/database/types/LegacyExportDataVersions/LegacyExportDataV1_0_0.ts
+++ app/assets/js/src/database/types/LegacyExportDataVersions/LegacyExportDataV1_0_0.ts
@@ -5,73 +5,76 @@ import { isZodSchemaType } from 'utils';
 import { legacyStatusNameSchema } from './LegacyStatusName';
 
 export const legacyExportDataSchemaV1_0_0 = z.object({
-	data: z.object({
-		days: z.array(
-			z.tuple([
-				z.string(),
-				z.object({
-					name: z.string(),
-					note: z.string(),
-					tasks: z.readonly(z.array(z.number())),
-				}),
-			]),
-		),
-		tasks: z.array(
-			z.tuple([
-				z.number(),
-				z.union([
+	data: z.union([
+		z.record(z.string(), z.never()),
+		z.object({
+			days: z.array(
+				z.tuple([
+					z.string(),
 					z.object({
-						id: z.number(),
 						name: z.string(),
-						status: legacyStatusNameSchema,
-					}),
-					z.object({
-						id: z.number(),
-						name: z.string(),
-						status: legacyStatusNameSchema,
 						note: z.string(),
+						tasks: z.readonly(z.array(z.number())),
 					}),
+				]),
+			),
+			tasks: z.array(
+				z.tuple([
+					z.number(),
+					z.union([
+						z.object({
+							id: z.number(),
+							name: z.string(),
+							status: legacyStatusNameSchema,
+						}),
+						z.object({
+							id: z.number(),
+							name: z.string(),
+							status: legacyStatusNameSchema,
+							note: z.string(),
+						}),
+						z.object({
+							id: z.number(),
+							name: z.string(),
+							status: legacyStatusNameSchema,
+							note: z.string(),
+							sortIndex: z.number(),
+						}),
+					]),
+				]),
+			),
+			['day-tasks']: z.array(
+				z.tuple([
+					z.stringFormat('dayTask identifier', (val) => /\d{4}-\d{2}-\d{2}_\d+/.test(val)),
+					z.union([
+						z.object({
+							dayName: z.string(),
+							taskId: z.number(),
+							status: legacyStatusNameSchema,
+							note: z.string(),
+							summary: z.nullable(z.string()),
+						}),
+						z.object({
+							status: legacyStatusNameSchema,
+							note: z.string(),
+							summary: z.string(),
+						}),
+					]),
+				]),
+			),
+			templates: z.optional(z.array(
+				z.tuple([
+					z.number(),
 					z.object({
 						id: z.number(),
 						name: z.string(),
-						status: legacyStatusNameSchema,
-						note: z.string(),
+						template: z.string(),
 						sortIndex: z.number(),
 					}),
 				]),
-			]),
-		),
-		['day-tasks']: z.array(
-			z.tuple([
-				z.stringFormat('dayTask identifier', (val) => /\d{4}-\d{2}-\d{2}_\d+/.test(val)),
-				z.union([
-					z.object({
-						dayName: z.string(),
-						taskId: z.number(),
-						status: legacyStatusNameSchema,
-						note: z.string(),
-						summary: z.nullable(z.string()),
-					}),
-					z.object({
-						status: legacyStatusNameSchema,
-						note: z.string(),
-						summary: z.string(),
-					}),
-				]),
-			]),
-		),
-		templates: z.optional(z.array(
-			z.tuple([
-				z.number(),
-				z.object({
-					id: z.number(),
-					name: z.string(),
-					template: z.string(),
-					sortIndex: z.number(),
-				}),
-			]),
-		)),
-	}),
+			)),
+		}),
+	]),
 	images: z.optional(
 		z.record(
 			z.string(),
