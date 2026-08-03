c412927fcbfe6b27f9b376628ca4741e9e028f11
Brooke Hart <brooke+github@curly.kiwi>
2026-08-04 07:33:11 +1200

    Added day task note saving

--- app/assets/js/src/types/SaveAction.ts
+++ app/assets/js/src/types/SaveAction.ts
@@ -2,12 +2,25 @@ import type { EnumTypeOf, ExpandType } from 'utils';
 
 export const SaveType = {
 	TASK_NOTE: 'task note',
+
+	// TODO: Remove this once day task notes can be saved via the day task's ID
+	DAY_TASK_NOTE_LEGACY: 'day task note (legacy)',
+	DAY_TASK_NOTE: 'day task note',
 } as const;
 export type SaveType = EnumTypeOf<typeof SaveType>;
 
 interface SaveActionByType {
 	[SaveType.TASK_NOTE]: {
 		task: number;
+		note: string;
+	};
+	[SaveType.DAY_TASK_NOTE_LEGACY]: {
+		dayName: string;
+		taskId: number;
+		note: string;
+	};
+	[SaveType.DAY_TASK_NOTE]: {
+		dayTask: number;
 		note: string;
 	};
 }
