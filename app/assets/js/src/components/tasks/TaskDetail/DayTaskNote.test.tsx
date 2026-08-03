301ac32509f3d644f2f656acd5f7b5cf27518792
Brooke Hart <brooke+github@curly.kiwi>
2026-08-04 07:33:51 +1200

    Added day task saving

---
+++ app/assets/js/src/components/tasks/TaskDetail/DayTaskNote.test.tsx
@@ -0,0 +1,104 @@
+import { h } from 'preact';
+
+import {
+	beforeAll,
+	describe,
+	expect,
+	jest,
+	test,
+} from '@jest/globals';
+import '@testing-library/jest-dom/jest-globals';
+
+import { cleanup, render } from '@testing-library/preact';
+import userEvent from '@testing-library/user-event';
+
+import { Command } from 'types/Command';
+import { addCommandListener, registerCommand } from 'registers/commands';
+
+import { clear } from 'data';
+
+import { SaveType } from 'types/SaveAction';
+
+import { OrangeTwistContext } from 'components/OrangeTwistContext';
+
+import { DayTaskNote } from './DayTaskNote';
+import { afterEach, beforeEach } from 'node:test';
+
+describe('DayTaskNote', () => {
+	beforeAll(() => {
+		registerCommand(Command.DATA_SAVE, { name: 'Save data' });
+	});
+
+	beforeEach(() => {
+		clear();
+	});
+
+	afterEach(() => {
+		cleanup();
+	});
+
+	test('renders the day task\'s note', () => {
+		const { getByText } = render(<OrangeTwistContext.Provider
+			value={{
+				isLoading: false,
+			}}
+		>
+			<DayTaskNote
+				dayTask={{
+					dayName: '2026-08-04',
+					taskId: 1,
+					summary: 'Summary',
+					note: 'Day task note',
+					status: 'todo',
+				}}
+			/>
+		</OrangeTwistContext.Provider>);
+
+		expect(getByText('Day task note')).toBeInTheDocument();
+	});
+
+	test('saves note after change', async () => {
+		const controller = new AbortController();
+		const { signal } = controller;
+
+		const user = userEvent.setup();
+
+		const spy = jest.fn();
+
+		addCommandListener(Command.DATA_SAVE, spy, { signal });
+
+		const { getAllByRole } = render(<OrangeTwistContext.Provider
+			value={{
+				isLoading: false,
+			}}
+		>
+			<DayTaskNote
+				dayTask={{
+					dayName: '2026-08-04',
+					taskId: 1,
+					summary: 'Summary',
+					note: 'Day task note',
+					status: 'todo',
+				}}
+			/>
+		</OrangeTwistContext.Provider>);
+
+		const noteEditButton = getAllByRole('button', { name: 'Edit note' })[0];
+		await user.click(noteEditButton);
+		await user.keyboard(' edited');
+
+		expect(spy).not.toHaveBeenCalled();
+
+		await user.click(document.body);
+
+		expect(spy).toHaveBeenCalledTimes(1);
+		expect(spy).toHaveBeenCalledWith([{
+			type: SaveType.DAY_TASK_NOTE_LEGACY,
+			dayName: '2026-08-04',
+			taskId: 1,
+			note: 'Day task note edited',
+		}]);
+
+		controller.abort();
+	});
+});
