08e1e02da501e620700bf720c2d1fbcefdd8854f
Brooke Hart <brooke+github@curly.kiwi>
2026-08-12 08:31:05 +1200

    Fixed failing test after removing status from task

--- app/assets/js/src/database/utils/index.ts
+++ app/assets/js/src/database/utils/index.ts
@@ -2,3 +2,4 @@ export { getDatabase } from './getDatabase';
 export { getDatabaseVersion } from './getDatabaseVersion';
 export { getDayName } from './getDayName';
 export { getDayNameParts } from './getDayNameParts';
+export { sortDaysChronologically } from './sortDaysChronologically';
---
+++ app/assets/js/src/database/utils/sortDaysChronologically.test.ts
@@ -0,0 +1,5 @@
+import { describe, test } from '@jest/globals';
+
+describe('sortDaysChronologically', () => {
+	test.todo('sorts two days chronologically');
+});
