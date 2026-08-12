81d45f6fd378c43fa7a384d3ef2d1ec189f27d0e
Brooke Hart <brooke+github@curly.kiwi>
2026-08-12 23:02:53 +1200

    Added test suite for sortDaysChronologically

--- app/assets/js/src/database/utils/sortDaysChronologically.test.ts
+++ app/assets/js/src/database/utils/sortDaysChronologically.test.ts
@@ -1,5 +1,51 @@
-import { describe, test } from '@jest/globals';
+import {
+	describe,
+	expect,
+	test,
+} from '@jest/globals';
+
+import { sortDaysChronologically } from './sortDaysChronologically';
 
 describe('sortDaysChronologically', () => {
-	test.todo('sorts two days chronologically');
+	test('sorts two days chronologically', () => {
+		const dayA = {
+			year: 2026,
+			month: 8,
+			day: 11,
+		};
+		const dayB = {
+			year: 2026,
+			month: 8,
+			day: 12,
+		};
+		const dayC = {
+			year: 2026,
+			month: 9,
+			day: 10,
+		};
+		const dayD = {
+			year: 2027,
+			month: 1,
+			day: 12,
+		};
+
+		expect(
+			sortDaysChronologically(dayA, dayA)
+		).toBe(0);
+
+
+		expect(
+			sortDaysChronologically(dayA, dayB)
+		).toBe(-1);
+
+
+		expect(
+			sortDaysChronologically(dayA, dayC)
+		).toBe(-1);
+
+
+		expect(
+			sortDaysChronologically(dayA, dayD)
+		).toBe(-1);
+	});
 });
