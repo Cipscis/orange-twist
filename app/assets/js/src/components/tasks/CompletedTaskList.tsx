0f126a035725242021d3e94971cc62f03775c569
Brooke Hart <brooke+github@curly.kiwi>
2026-08-31 15:35:46 +1200

    Streamline sorting of completed tasks

--- app/assets/js/src/components/tasks/CompletedTaskList.tsx
+++ app/assets/js/src/components/tasks/CompletedTaskList.tsx
@@ -13,6 +13,7 @@ import {
 	useAllTaskInfo,
 	type TaskInfo,
 } from 'data';
+
 import { sortElementsBySortIndex } from 'utils';
 
 import { Accordion } from 'components/shared';
@@ -67,15 +68,6 @@ export function CompletedTaskList(props: CompletedTaskListProps): JSX.Element | 
 		return sortedTasks.map(([{ id }]) => id);
 	}, [
 		matchingTaskInfo,
-	]);
-
-	const matchingTaskInfo = useAllTaskInfo(matcher);
-	const sortedTaskIds = useMemo(() => {
-		const sortedTasks = matchingTaskInfo.toSorted(sorter);
-
-		return sortedTasks.map(({ id }) => id);
-	}, [
-		matchingTaskInfo, sorter,
 	]);
 
 	// Update list open state if prop changes
