d3ea3f838d790544117ed94edb68c4283e0fe321
Brooke Hart <brooke+github@curly.kiwi>
2026-09-15 18:52:10 +1200

    Use immediate option in settable async data

--- app/assets/js/src/database/access/hooks/useSettableTask.ts
+++ app/assets/js/src/database/access/hooks/useSettableTask.ts
@@ -47,22 +47,8 @@ export function useSettableTask(taskId: number): ExpandType<
 		getData: getTask,
 		setData: setTask,
 		optimistic: true,
+		immediate: true,
 	});
-
-	// Fetch data immediately on initial load
-	useEffect(
-		() => {
-			const controller = new AbortController();
-			const { signal } = controller;
-
-			asyncDataResult.getData({ signal });
-
-			return () => controller.abort();
-		},
-		// Deliberately only fetch data (or abort prior fetches) if `getTask` changes
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-		[getTask]
-	);
 
 	// Re-fetch the data if it changes
 	useEffect(() => {
