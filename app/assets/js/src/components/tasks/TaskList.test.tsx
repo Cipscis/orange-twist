91218b40810a13ba7a5f3826c2d08a9717f8c30e
Brooke Hart <brooke+github@curly.kiwi>
2026-09-17 15:33:53 +1200

    Use insertTestData for TaskList test

--- app/assets/js/src/components/tasks/TaskList.test.tsx
+++ app/assets/js/src/components/tasks/TaskList.test.tsx
@@ -11,36 +11,77 @@ import '@testing-library/jest-dom/jest-globals';
 
 import { cleanup, render } from '@testing-library/preact';
 
-import { TaskStatus } from 'types/TaskStatus';
-import { clear, setTaskInfo } from 'data';
+import { insertTestData } from 'database';
 
 import { TaskList } from './TaskList';
 
 describe('TaskList', () => {
-	beforeEach(() => {
-		clear();
-
-		setTaskInfo(1, {
-			name: 'Task one',
-			status: TaskStatus.IN_PROGRESS,
-			sortIndex: 1,
-		});
-		setTaskInfo(2, {
-			name: 'Task two',
-			status: TaskStatus.COMPLETED,
-			sortIndex: 3,
-		});
-		setTaskInfo(3, {
-			name: 'Task three',
-			status: TaskStatus.INVESTIGATING,
-			sortIndex: 2,
-		});
-		setTaskInfo(4, {
-			name: 'Task four',
-			status: TaskStatus.COMPLETED,
-			sortIndex: -1,
-		});
-	});
+	beforeEach(() => insertTestData({
+		task: {
+			1: {
+				id: 1,
+				name: 'Task one',
+				note: '',
+				sortIndex: 1,
+			},
+			2: {
+				id: 2,
+				name: 'Task two',
+				note: '',
+				sortIndex: 3,
+			},
+			3: {
+				id: 3,
+				name: 'Task three',
+				note: '',
+				sortIndex: 2,
+			},
+			4: {
+				id: 4,
+				name: 'Task four',
+				note: '',
+				sortIndex: -1,
+			},
+		},
+		day_task: {
+			1: {
+				id: 1,
+				day: 1,
+				task: 1,
+				status: 2,
+				note: '',
+				summary: null,
+				sortIndex: 1,
+			},
+			2: {
+				id: 2,
+				day: 1,
+				task: 2,
+				status: 3,
+				note: '',
+				summary: null,
+				sortIndex: 1,
+			},
+			3: {
+				id: 3,
+				day: 1,
+				task: 3,
+				status: 4,
+				note: '',
+				summary: null,
+				sortIndex: 1,
+			},
+			4: {
+				id: 4,
+				day: 1,
+				task: 4,
+				status: 3,
+				note: '',
+				summary: null,
+				sortIndex: 1,
+			},
+		},
+	}));
 
 	afterEach(() => {
 		cleanup();
