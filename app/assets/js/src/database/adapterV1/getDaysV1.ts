9fb0b0a93170c81405febd01643763c54b53792e
Brooke Hart <brooke+github@curly.kiwi>
2026-09-18 20:27:02 +1200

    Parallelise downgrading of days

--- app/assets/js/src/database/adapterV1/getDaysV1.ts
+++ app/assets/js/src/database/adapterV1/getDaysV1.ts
@@ -1,5 +1,6 @@
 import type { DayInfo } from 'data/days';
 
+import type { Day } from '../types';
 import { getDatabase, getDayName } from '../utils';
 import { ObjectStoreName } from '../metadata';
 import { getDaysInternal, getDayTasksForDayInternal } from '../internal';
@@ -8,7 +9,6 @@ import { getDaysInternal, getDayTasksForDayInternal } from '../internal';
  * Retrieve all schema v1 {@linkcode DayInfo} information from the database v2.
  */
 export async function getDaysV1(): Promise<[string, DayInfo][]> {
-	const daysV1: DayInfo[] = [];
 
 	const db = await getDatabase();
 	const transaction = db.transaction([
@@ -17,17 +17,27 @@ export async function getDaysV1(): Promise<[string, DayInfo][]> {
 	], 'readonly');
 
 	const allDays = await getDaysInternal(transaction);
-	for (const day of allDays) {
-		const dayTasks = await getDayTasksForDayInternal(transaction, day.id);
-
-		const dayV1: DayInfo = {
-			name: getDayName(day),
-			note: day.note,
-			tasks: dayTasks.map((dayTask) => dayTask.task),
-		};
-
-		daysV1.push(dayV1);
-	}
+	const daysV1 = await Promise.all(
+		allDays.map((day) => downgradeDay(day, transaction))
+	);
 
 	return daysV1.map((day) => [day.name, day]);
+}
+
+/**
+ * Downgrade a {@linkcode Day} from the database v2 into a {@linkcode DayInfo} from the database v1, which includes a list of tasks with day tasks against this day.
+ */
+async function downgradeDay(
+	day: Day,
+	transaction: IDBTransaction
+): Promise<DayInfo> {
+	const dayTasks = await getDayTasksForDayInternal(transaction, day.id);
+
+	const dayV1: DayInfo = {
+		name: getDayName(day),
+		note: day.note,
+		tasks: dayTasks.map((dayTask) => dayTask.task),
+	};
+
+	return dayV1;
 }
